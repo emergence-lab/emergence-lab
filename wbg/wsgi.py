514bf12e76c47b8575649e49f75d20a35204ff99
Jonathan Marini <j.marini@ieee.org>
2014-01-15 11:12:02 -0500

    Moved wsgi config to wbg/wsgi.py

--- wbg/wsgi.py
+++ wbg/wsgi.py
@@ -6,9 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wbg.settings")
+import sys
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+
+
+sys.path.append('/var/wsgi')
+sys.path.append('/var/wsgi/wbg')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wbg.settings')
+
+application = django.core.handlers.wsgi.WSGIHandler()
