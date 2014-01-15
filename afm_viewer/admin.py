ac7cfe0ec664a06dc4be6a8bbc8f0bfe25b710e1
Jonathan Marini <j.marini@ieee.org>
2014-01-15 12:58:16 -0500

    Added models to admin interface

--- afm_viewer/admin.py
+++ afm_viewer/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import operator, platter, project, growth, afm
 
-# Register your models here.
+
+class growth_admin(admin.ModelAdmin):
+    fields = ['growth_number', 'date', 'operator', 'project', 'investigation',
+              'run_comments', 'carrier_number']
+
+
+admin.site.register(operator)
+admin.site.register(platter)
+admin.site.register(project)
+admin.site.register(growth, growth_admin)
+admin.site.register(afm)
