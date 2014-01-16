83586bafeeb1b362198cc2ae58741ec1743258c4
Jonathan Marini <j.marini@ieee.org>
2014-01-16 13:25:44 -0500

    Cleaned up models.py according to pep8.

--- core/models.py
+++ core/models.py
@@ -11,7 +11,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class operator( models.Model ):
+
+class operator(models.Model):
     name = models.CharField(max_length=45)
     active = models.BooleanField(default=True)
 
@@ -21,10 +22,11 @@ class operator( models.Model ):
     class Meta:
         db_table = 'operators'
 
-class platter( models.Model ):
+
+class platter(models.Model):
     name = models.CharField(max_length=45)
     active = models.BooleanField(default=True)
-    serial = models.CharField(max_length=20,blank=True)
+    serial = models.CharField(max_length=20, blank=True)
     start_date = models.DateField(auto_now_add=True)
     end_date = models.DateField(blank=True, null=True)
 
@@ -35,7 +37,7 @@ class platter( models.Model ):
         db_table = 'platters'
 
 
-class project( models.Model ):
+class project(models.Model):
     name = models.CharField(max_length=45)
     active = models.BooleanField(default=True)
 
@@ -58,85 +60,85 @@ class investigation(models.Model):
         db_table = 'investigations'
 
 
-class growth( models.Model ):
-    id = models.IntegerField( primary_key=True )
-    growth_number = models.SlugField( max_length=10, blank=True, db_index=True )
-    date = models.DateField( blank=True, null=True )
-    operator = models.CharField( max_length=45, blank=True )
-    project = models.CharField( max_length=45, blank=True )
-    investigation = models.CharField( max_length=100, blank=True )
-    reactor_select = models.CharField( max_length=45, blank=True )
-    run_comments = models.CharField( max_length=1000, blank=True )
-    carrier_number = models.CharField( max_length=45, blank=True )
+class growth(models.Model):
+    id = models.IntegerField(primary_key=True)
+    growth_number = models.SlugField(max_length=10)
+    date = models.DateField(blank=True, null=True)
+    operator = models.CharField(max_length=45, blank=True)
+    project = models.CharField(max_length=45, blank=True)
+    investigation = models.CharField(max_length=100, blank=True)
+    reactor_select = models.CharField(max_length=45, blank=True)
+    run_comments = models.CharField(max_length=1000, blank=True)
+    carrier_number = models.CharField(max_length=45, blank=True)
 
     # pocket
-    p1_sub_number = models.CharField( max_length=45, blank=True )
-    p1_run_number = models.CharField( max_length=45, blank=True )
-    p1_polish = models.CharField( max_length=45, blank=True )
-    p2_sub_number = models.CharField( max_length=45, blank=True )
-    p2_run_number = models.CharField( max_length=45, blank=True )
-    p2_polish = models.CharField( max_length=45, blank=True )
-    p3_sub_number = models.CharField( max_length=45, blank=True )
-    p3_run_number = models.CharField( max_length=45, blank=True )
-    p3_polish = models.CharField( max_length=45, blank=True )
-    p4_sub_number = models.CharField( max_length=45, blank=True )
-    p4_run_number = models.CharField( max_length=45, blank=True )
-    p4_polish = models.CharField( max_length=45, blank=True )
-    p5_sub_number = models.CharField( max_length=45, blank=True )
-    p5_run_number = models.CharField( max_length=45, blank=True )
-    p5_polish = models.CharField( max_length=45, blank=True )
-    p6_sub_number = models.CharField( max_length=45, blank=True )
-    p6_run_number = models.CharField( max_length=45, blank=True )
-    p6_polish = models.CharField( max_length=45, blank=True )
+    p1_sub_number = models.CharField(max_length=45, blank=True)
+    p1_run_number = models.CharField(max_length=45, blank=True)
+    p1_polish = models.CharField(max_length=45, blank=True)
+    p2_sub_number = models.CharField(max_length=45, blank=True)
+    p2_run_number = models.CharField(max_length=45, blank=True)
+    p2_polish = models.CharField(max_length=45, blank=True)
+    p3_sub_number = models.CharField(max_length=45, blank=True)
+    p3_run_number = models.CharField(max_length=45, blank=True)
+    p3_polish = models.CharField(max_length=45, blank=True)
+    p4_sub_number = models.CharField(max_length=45, blank=True)
+    p4_run_number = models.CharField(max_length=45, blank=True)
+    p4_polish = models.CharField(max_length=45, blank=True)
+    p5_sub_number = models.CharField(max_length=45, blank=True)
+    p5_run_number = models.CharField(max_length=45, blank=True)
+    p5_polish = models.CharField(max_length=45, blank=True)
+    p6_sub_number = models.CharField(max_length=45, blank=True)
+    p6_run_number = models.CharField(max_length=45, blank=True)
+    p6_polish = models.CharField(max_length=45, blank=True)
 
     # growth tags
-    layerhas_gan = models.IntegerField( blank=True, null=True )
-    layerhas_algan = models.IntegerField( blank=True, null=True )
-    layerhas_ingan = models.IntegerField( blank=True, null=True )
-    layerhas_aln = models.IntegerField( blank=True, null=True )
-    layerhas_other = models.CharField( max_length=45, blank=True )
-    layeris_ntype = models.IntegerField( blank=True, null=True )
-    layeris_ptype = models.IntegerField( blank=True, null=True )
-    layeris_uid = models.IntegerField( blank=True, null=True )
-    substrate_orient_c = models.IntegerField( blank=True, null=True )
-    substrate_orient_a = models.IntegerField( blank=True, null=True )
-    substrate_orient_m = models.IntegerField( blank=True, null=True )
-    substrateis_orient_r = models.IntegerField( blank=True, null=True )
-    substrate_orient_other = models.IntegerField( blank=True, null=True )
-    is_template = models.IntegerField( blank=True, null=True )
-    substrate_is = models.CharField( max_length=45, blank=True )
-    layerhas_superlattice = models.IntegerField( blank=True, null=True )
-    layerhas_mqw = models.IntegerField( blank=True, null=True )
-    layerhas_sqw = models.IntegerField( blank=True, null=True) 
-    film_orient_c = models.IntegerField( blank=True, null=True )
-    film_orient_a = models.IntegerField( blank=True, null=True )
-    film_orient_m = models.IntegerField( blank=True, null=True )
-    film_orient_r = models.IntegerField( blank=True, null=True )
-    film_orient_other = models.IntegerField( blank=True, null=True )
+    layerhas_gan = models.IntegerField(blank=True, null=True)
+    layerhas_algan = models.IntegerField(blank=True, null=True)
+    layerhas_ingan = models.IntegerField(blank=True, null=True)
+    layerhas_aln = models.IntegerField(blank=True, null=True)
+    layerhas_other = models.CharField(max_length=45, blank=True)
+    layeris_ntype = models.IntegerField(blank=True, null=True)
+    layeris_ptype = models.IntegerField(blank=True, null=True)
+    layeris_uid = models.IntegerField(blank=True, null=True)
+    substrate_orient_c = models.IntegerField(blank=True, null=True)
+    substrate_orient_a = models.IntegerField(blank=True, null=True)
+    substrate_orient_m = models.IntegerField(blank=True, null=True)
+    substrateis_orient_r = models.IntegerField(blank=True, null=True)
+    substrate_orient_other = models.IntegerField(blank=True, null=True)
+    is_template = models.IntegerField(blank=True, null=True)
+    substrate_is = models.CharField(max_length=45, blank=True)
+    layerhas_superlattice = models.IntegerField(blank=True, null=True)
+    layerhas_mqw = models.IntegerField(blank=True, null=True)
+    layerhas_sqw = models.IntegerField(blank=True, null=True)
+    film_orient_c = models.IntegerField(blank=True, null=True)
+    film_orient_a = models.IntegerField(blank=True, null=True)
+    film_orient_m = models.IntegerField(blank=True, null=True)
+    film_orient_r = models.IntegerField(blank=True, null=True)
+    film_orient_other = models.IntegerField(blank=True, null=True)
 
     # checklist / run logging
     chk_pre_rcp = models.IntegerField(blank=True, null=True)
-    chk_pre_openll = models.IntegerField(db_column='chk_pre_openLL', blank=True, null=True) # Field name made lowercase.
+    chk_pre_openll = models.IntegerField(db_column='chk_pre_openLL', blank=True, null=True)  # Field name made lowercase.
     chk_pre_load = models.IntegerField(blank=True, null=True)
-    chk_pre_closell = models.IntegerField(db_column='chk_pre_closeLL', blank=True, null=True) # Field name made lowercase.
+    chk_pre_closell = models.IntegerField(db_column='chk_pre_closeLL', blank=True, null=True)  # Field name made lowercase.
     chk_pre_alk = models.IntegerField(blank=True, null=True)
     chk_pre_hyd = models.IntegerField(blank=True, null=True)
-    chk_pre_llpres = models.DecimalField(db_column='chk_pre_LLpres', max_digits=2, decimal_places=1, blank=True, null=True) # Field name made lowercase.
-    chk_pre_opengv = models.IntegerField(db_column='chk_pre_openGV', blank=True, null=True) # Field name made lowercase.
+    chk_pre_llpres = models.DecimalField(db_column='chk_pre_LLpres', max_digits=2, decimal_places=1, blank=True, null=True)  # Field name made lowercase.
+    chk_pre_opengv = models.IntegerField(db_column='chk_pre_openGV', blank=True, null=True)  # Field name made lowercase.
     chk_pre_trans = models.IntegerField(blank=True, null=True)
     chk_pre_rot = models.IntegerField(blank=True, null=True)
-    chk_pre_closegv = models.IntegerField(db_column='chk_pre_closeGV', blank=True, null=True) # Field name made lowercase.
+    chk_pre_closegv = models.IntegerField(db_column='chk_pre_closeGV', blank=True, null=True)  # Field name made lowercase.
     chk_pre_final = models.IntegerField(blank=True, null=True)
     chk_pre_epi = models.IntegerField(blank=True, null=True)
     chk_start = models.IntegerField(blank=True, null=True)
     chk_post_idle = models.IntegerField(blank=True, null=True)
     chk_post_episave = models.IntegerField(blank=True, null=True)
-    chk_post_opengv = models.IntegerField(db_column='chk_post_openGV', blank=True, null=True) # Field name made lowercase.
+    chk_post_opengv = models.IntegerField(db_column='chk_post_openGV', blank=True, null=True)  # Field name made lowercase.
     chk_post_trans = models.IntegerField(blank=True, null=True)
-    chk_post_closegv = models.IntegerField(db_column='chk_post_closeGV', blank=True, null=True) # Field name made lowercase.
-    chk_post_llpres = models.DecimalField(db_column='chk_post_LLpres', max_digits=2, decimal_places=1, blank=True, null=True) # Field name made lowercase.
-    chk_post_openll = models.IntegerField(db_column='chk_post_openLL', blank=True, null=True) # Field name made lowercase.
-    chk_post_closell = models.IntegerField(db_column='chk_post_closeLL', blank=True, null=True) # Field name made lowercase.
+    chk_post_closegv = models.IntegerField(db_column='chk_post_closeGV', blank=True, null=True)  # Field name made lowercase.
+    chk_post_llpres = models.DecimalField(db_column='chk_post_LLpres', max_digits=2, decimal_places=1, blank=True, null=True)  # Field name made lowercase.
+    chk_post_openll = models.IntegerField(db_column='chk_post_openLL', blank=True, null=True)  # Field name made lowercase.
+    chk_post_closell = models.IntegerField(db_column='chk_post_closeLL', blank=True, null=True)  # Field name made lowercase.
     chk_post_alk = models.IntegerField(blank=True, null=True)
 
     cl_1 = models.IntegerField(blank=True, null=True)
@@ -379,20 +381,20 @@ class growth( models.Model ):
         db_table = 'growths'
 
 
-class afm( models.Model ):
-    id = models.IntegerField( primary_key=True )
-    growth = models.ForeignKey( growth )
-    growth_number = models.CharField( max_length=20 )
-    pocket = models.IntegerField( default=1 )
-    scan_number = models.IntegerField( default=0 )
+class afm(models.Model):
+    id = models.IntegerField(primary_key=True)
+    growth = models.ForeignKey(growth)
+    growth_number = models.CharField(max_length=20)
+    pocket = models.IntegerField(default=1)
+    scan_number = models.IntegerField(default=0)
 
-    rms = models.DecimalField( max_digits=7, decimal_places=3, blank=True, null=True )
-    zrange = models.DecimalField( max_digits=7, decimal_places=3, blank=True, null=True )
-    location = models.CharField( max_length=45, blank=True )
-    size = models.DecimalField( max_digits=7, decimal_places=3, blank=True, null=True )
+    rms = models.DecimalField(max_digits=7, decimal_places=3, blank=True, null=True)
+    zrange = models.DecimalField(max_digits=7, decimal_places=3, blank=True, null=True)
+    location = models.CharField(max_length=45, blank=True)
+    size = models.DecimalField(max_digits=7, decimal_places=3, blank=True, null=True)
 
-    filename = models.CharField( max_length=150, blank=True)
-    amplitude_filename = models.CharField( max_length=150, blank=True )
+    filename = models.CharField(max_length=150, blank=True)
+    amplitude_filename = models.CharField(max_length=150, blank=True)
 
     def __unicode__(self):
         return '{0}_{1}_{2}.{3}'.format(self.growth_number, self.pocket, self.location.lower()[0],
@@ -400,4 +402,3 @@ class afm( models.Model ):
 
     class Meta:
         db_table = 'afm'
-
