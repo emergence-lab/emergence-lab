027f4af48bbc9b76a299eb863552fe07a423ab1f
Jonathan Marini <j.marini@ieee.org>
2014-01-23 16:13:09 -0500

    Added interface to compare the afms selected in filter view. Currently lists simple info about them.

--- growths/views.py
+++ growths/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from core.models import afm, growth
 from .filters import growth_filter, RelationalFilterView
@@ -8,6 +8,15 @@ from .filters import growth_filter, RelationalFilterView
 class growth_list(RelationalFilterView):
     filterset_class = growth_filter
     template_name = 'core/growth_filter.html'
+
+
+class afm_compare(ListView):
+    template_name = 'core/afm_compare.html'
+
+    def get_queryset(self):
+        id_list = [int(id) for id in self.request.GET.getlist('afm')]
+        objects = afm.objects.filter(id__in=id_list)
+        return objects
 
 
 class growth_detail(DetailView):
--- wbg/urls.py
+++ wbg/urls.py
@@ -13,5 +13,6 @@ urlpatterns = patterns(
     url(r'^$', growths.views.growth_list.as_view(), name='afm_filter'),
     url(r'^(?P<slug>[gt][1-9][0-9]{3,})/$', growths.views.growth_detail.as_view(), name='growth_detail'),
     url(r'^afm-(?P<pk>\d+)/$', growths.views.afm_detail.as_view(), name='afm_detail'),
+    url(r'^afm-compare/$', growths.views.afm_compare.as_view(), name='afm_compare'),
     url(r'^admin/', include(admin.site.urls)),
 )
