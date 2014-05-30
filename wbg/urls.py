c37b096f90597c5ae8f453a81ffd4ed8cbd50e2f
Jonathan Marini <j.marini@ieee.org>
2014-05-30 14:59:43 -0400

    Added initial support for LDAP login protected pages. Currently works on growth detail page.

--- wbg/urls.py
+++ wbg/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import login
+import django.contrib
 
 import growths.views
 
@@ -8,11 +10,12 @@ import growths.views
 admin.autodiscover()
 
 urlpatterns = patterns(
-    '',
+    'django.contrib.auth.views',
     # urls, add login_required() around the as_view() call for security
     url(r'^$', growths.views.growth_list.as_view(), name='afm_filter'),
-    url(r'^(?P<slug>[gt][1-9][0-9]{3,})/$', growths.views.growth_detail.as_view(), name='growth_detail'),
+    url(r'^(?P<slug>[gt][1-9][0-9]{3,})/$', login_required(growths.views.growth_detail.as_view()), name='growth_detail'),
     url(r'^afm-(?P<pk>\d+)/$', growths.views.afm_detail.as_view(), name='afm_detail'),
     url(r'^afm-compare/$', growths.views.afm_compare.as_view(), name='afm_compare'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/', login, {'template_name': 'core/login.html'})
 )
