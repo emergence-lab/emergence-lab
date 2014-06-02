881ff6a491b5d7898514ab9f4d9587599fa0c266
Jonathan Marini <j.marini@ieee.org>
2014-06-02 14:43:33 -0400

    Added homepage that links to afm filter and made all important pages require login.

--- wbg/urls.py
+++ wbg/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import login, logout
 import django.contrib
 
+import core.views
 import growths.views
 
 
@@ -12,10 +13,11 @@ admin.autodiscover()
 urlpatterns = patterns(
     'django.contrib.auth.views',
     # urls, add login_required() around the as_view() call for security
-    url(r'^$', growths.views.growth_list.as_view(), name='afm_filter'),
+    url(r'^$', core.views.homepage.as_view(), name='home'),
+    url(r'^afm-filter/$', login_required(growths.views.growth_list.as_view()), name='afm_filter'),
     url(r'^(?P<slug>[gt][1-9][0-9]{3,})/$', login_required(growths.views.growth_detail.as_view()), name='growth_detail'),
     url(r'^afm-(?P<pk>\d+)/$', growths.views.afm_detail.as_view(), name='afm_detail'),
-    url(r'^afm-compare/$', growths.views.afm_compare.as_view(), name='afm_compare'),
+    url(r'^afm-compare/$', login_required(growths.views.afm_compare.as_view()), name='afm_compare'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/', login, {'template_name': 'core/login.html'}, name='login'),
     url(r'^accounts/logout/', logout, {'template_name': 'core/logout.html'}, name='logout')
