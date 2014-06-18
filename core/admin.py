df98e81ab02cbaf66c577888fd4514e61e169079
Jonathan Marini <j.marini@ieee.org>
2014-06-18 13:06:11 -0400

    Added sample model to admin interface, reverted to default growth_admin page

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
-from .models import operator, platter, project, growth, afm, investigation
+import core.models
 
 
-class growth_admin(admin.ModelAdmin):
-    fields = ['growth_number', 'date', 'operator', 'project', 'investigation',
-              'run_comments', 'carrier_number']
-
-
-admin.site.register(operator)
-admin.site.register(platter)
-admin.site.register(project)
-admin.site.register(growth, growth_admin)
-admin.site.register(afm)
-admin.site.register(investigation)
+admin.site.register(core.models.operator)
+admin.site.register(core.models.platter)
+admin.site.register(core.models.project)
+admin.site.register(core.models.growth)
+admin.site.register(core.models.sample)
+admin.site.register(core.models.afm)
+admin.site.register(core.models.investigation)
