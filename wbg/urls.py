3b7556f1b6c62009c7704026cd4874bd28856eae
Jonathan Marini <j.marini@ieee.org>
2014-06-18 12:12:51 -0400

    Changed afm details location to /afm/#/ from /afm-#/

--- wbg/urls.py
+++ wbg/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns(
     url(r'^$', core.views.homepage.as_view(), name='home'),
     url(r'^afm-filter/$', login_required(growths.views.growth_list.as_view()), name='afm_filter'),
     url(r'^(?P<slug>[gt][1-9][0-9]{3,})/$', login_required(growths.views.growth_detail.as_view()), name='growth_detail'),
-    url(r'^afm-(?P<pk>\d+)/$', growths.views.afm_detail.as_view(), name='afm_detail'),
+    url(r'^afm/(?P<pk>\d+)/$', login_required(growths.views.afm_detail.as_view()), name='afm_detail'),
     url(r'^afm-compare/$', login_required(growths.views.afm_compare.as_view()), name='afm_compare'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/', login, {'template_name': 'core/login.html'}, name='login'),
