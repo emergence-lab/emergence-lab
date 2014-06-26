ae89205ec43a8d34250b2667f291cdb6cc99cdca
Andrew Wilkinson <apwilkin@buffalo.edu>
2014-06-26 12:07:35 -0400

    Modified model for hall to be datetime instead of date.

--- hall/models.py
+++ hall/models.py
@@ -7,7 +7,7 @@ class hall(models.Model):
     growth = models.ForeignKey(growths.models.growth)
     sample = models.ForeignKey(growths.models.sample)
     description = models.TextField(blank=True)
-    date = models.DateField()
+    date = models.DateTimeField()
     temperature = models.DecimalField(max_digits=7, decimal_places=2, blank=True, default=300.0)
     symmetry_factor = models.DecimalField(max_digits=7, decimal_places=2, blank=True, default=1.0)
     sheet_coefficient = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
