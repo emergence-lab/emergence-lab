97c9237631c3d4d1a050d17d1622418a713b23ee
Andrew Wilkinson <apwilkin@buffalo.edu>
2014-07-07 14:16:49 -0400

    Functioning update view with the growth model, but without the sample model.

--- growths/forms.py
+++ growths/forms.py
@@ -1,2 +1,14 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+from growth.models import growth, sample
+
+# Create the form class.
+# class sample_form(ModelForm):
+#     class Meta:
+#         model = growth
+#         fields = ['substrate_type', 'substrate_serial', 'substrate_orientation', 'substrate_miscut',
+#                   'size']
+# class growth_form(ModelForm):
+#     class Meta:
+#         model = sample
--- growths/views.py
+++ growths/views.py
@@ -30,7 +30,16 @@ class growth_detail(DetailView):
 class create_growth(CreateView):
     model = growths.models.growth
     template_name = 'growths/create_growth.html'
-
+#     form_class = Form1
+#     second_form_class = Form2
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(create_growth, self).get_context_data(**kwargs)
+#         if 'form' not in context:
+#             context['form'] = self.form_class(initial={'some_field': context['model'].some_field})
+#         if 'form2' not in context:
+#             context['form2'] = self.second_form_class(initial={'another_field': context['model'].another_field})
+#         return context
 
     def get_initial(self):
         num_items = 0
@@ -45,6 +54,28 @@ class create_growth(CreateView):
 
         return { 'growth_number': last,
                  'date': time.strftime("%Y-%m-%d") }
+
+#     def post(self, request, *args, **kwargs):
+#         # get the user instance
+#         self.object = self.get_object()
+#         # determine which form is being submitted
+#         # uses the name of the form's submit button
+#         if 'form' in request.POST:
+#             # get the primary form
+#             form_class = self.get_form_class()
+#             form_name = 'form'
+#         else:
+#             # get the secondary form
+#             form_class = self.second_form_class
+#             form_name = 'form2'
+#          # get the form
+#         form = self.get_form(form_class)
+#          # validate
+#         if form.is_valid():
+#             return self.form_valid(form)
+#         else:
+#             return self.form_invalid(**{form_name: form})
+
 
     def get_success_url(self):
         return reverse('home')
