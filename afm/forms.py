121e5702e05e4cdc97391fd1f5b29377599355e5
Jonathan Marini <j.marini@ieee.org>
2014-07-15 11:06:24 -0400

    Updated create afm to use a charfield for growths as well.

--- afm/forms.py
+++ afm/forms.py
@@ -1,10 +1,10 @@
 import re
 
 from django import forms
-from django.core.exceptions import MultipleObjectsReturned
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 from .models import afm
-from growths.models import sample
+from growths.models import growth, sample
 import growths.models
 
 
@@ -12,9 +12,33 @@ class afm_form(forms.ModelForm):
     """
     Form for afm creation.
     """
+    growth = forms.CharField()
     sample = forms.CharField()
 
+    def clean_growth(self):
+        """
+        Validate growth number from user input and put a growth object in the form data.
+        """
+        growth_number = self.cleaned_data['growth']
+
+        m = re.match('[gt][1-9][0-9]{3,}', growth_number)
+        if not m:
+            raise forms.ValidationError('Growth {0} improperly formatted'.format(growth_number))
+
+        try:
+            self.cleaned_data['growth'] = growth.objects.get(growth_number=growth_number)
+        except MultipleObjectsReturned as e:
+            raise forms.ValidationError('Growth {0} ambiguous'.format(growth_number))
+        except ObjectDoesNotExist as e:
+            raise forms.ValidationError('Growth {0} does not exist'.format(growth_number))
+
+        return self.cleaned_data['growth']
+
     def clean_sample(self):
+        """
+        Validate sample number from user input and put a sample object in the form data.
+        """
+
         sample_name = self.cleaned_data['sample']
 
         # extract information from sample name
@@ -23,7 +47,7 @@ class afm_form(forms.ModelForm):
             raise forms.ValidationError('Sample {0} improperly formatted'.format(sample_name))
 
         # check if growth number and sample growth number match
-        if m.group(1) != self.cleaned_data['growth'].growth_number:
+        if 'growth' not in self.cleaned_data or m.group(1) != self.cleaned_data['growth'].growth_number:
             raise forms.ValidationError('Sample {0} does not match the growth'.format(sample_name))
         kwargs = {'growth': self.cleaned_data['growth']}
 
@@ -38,7 +62,7 @@ class afm_form(forms.ModelForm):
             self.cleaned_data['sample'] = sample.objects.get(**kwargs)
         except MultipleObjectsReturned as e:
             raise forms.ValidationError('Sample {0} ambiguous, specify the pocket or piece'.format(sample_name))
-        except DoesNotExist as e:
+        except ObjectDoesNotExist as e:
             raise forms.ValidationError('Sample {0} does not exist'.format(sample_name))
 
         return self.cleaned_data['sample']
