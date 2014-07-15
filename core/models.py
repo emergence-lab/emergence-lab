2ed2db7463def185b653417fe38fb8746482cdfd
Jonathan Marini <j.marini@ieee.org>
2014-07-15 12:12:39 -0400

    Added custom managers to models in core, to retrieve active (current) and inactive (retired) objects.

--- core/models.py
+++ core/models.py
@@ -1,11 +1,35 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
+
+
+class active_manager(models.Manager):
+    """
+    Manager to filter on the ``active`` field.
+    """
+    def get_queryset(self):
+        return super(active_manager, self).get_queryset().filter(active=True)
+
+
+class inactive_manager(models.Manager):
+    """
+    Manager to filter on the ``active`` field.
+    """
+    def get_queryset(self):
+        return super(inactive_manager, self).get_queryset().filter(active=False)
 
 
 class operator(models.Model):
+    """
+    Stores operator information.
+    """
     name = models.CharField(max_length=45)
     active = models.BooleanField(default=True)
+
+    objects = models.Manager()
+    current = active_manager()
+    retired = inactive_manager()
 
     def __unicode__(self):
         return self.name
@@ -15,11 +39,18 @@ class operator(models.Model):
 
 
 class platter(models.Model):
+    """
+    Stores platter information.
+    """
     name = models.CharField(max_length=45)
     active = models.BooleanField(default=True)
     serial = models.CharField(max_length=20, blank=True)
     start_date = models.DateField(auto_now_add=True)
     end_date = models.DateField(blank=True, null=True)
+
+    objects = models.Manager()
+    current = active_manager()
+    retired = inactive_manager()
 
     def __unicode__(self):
         return self.name
@@ -29,8 +60,15 @@ class platter(models.Model):
 
 
 class project(models.Model):
+    """
+    Stores information on a project, which is a higher level organizational tool.
+    """
     name = models.CharField(max_length=45)
     active = models.BooleanField(default=True)
+
+    objects = models.Manager()
+    current = active_manager()
+    retired = inactive_manager()
 
     def __unicode__(self):
         return self.name
@@ -40,9 +78,16 @@ class project(models.Model):
 
 
 class investigation(models.Model):
+    """
+    Stores information on an individual investigation related to one or more projects.
+    """
     name = models.CharField(max_length=45)
     active = models.BooleanField(default=True)
     projects = models.ManyToManyField(project)
+
+    objects = models.Manager()
+    current = active_manager()
+    retired = inactive_manager()
 
     def __unicode__(self):
         return self.name
