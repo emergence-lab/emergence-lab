3e91f340d7e26ef5bfda3321303ce1874602944c
Jonathan Marini <j.marini@ieee.org>
2014-07-16 09:34:48 -0400

    Broke out operator_list functionality into new class ActiveListView which assumes querysets named current and retired in the specified model and adds them to context under active_list and inactive_list

--- core/views.py
+++ core/views.py
@@ -4,6 +4,17 @@ from django.views.generic import TemplateView, ListView
 from .models import operator
 
 
+class ActiveListView(ListView):
+    """
+    View to handle models using the active and inactive manager.
+    """
+    def get_context_data(self, **kwargs):
+        context = super(ActiveListView, self).get_context_data(**kwargs)
+        context['active_list'] = self.model.current.all()
+        context['inactive_list'] = self.model.retired.all()
+        return context
+
+
 class homepage(TemplateView):
     """
     View for the homepage of the application.
@@ -11,15 +22,9 @@ class homepage(TemplateView):
     template_name = "core/index.html"
 
 
-class operator_list(ListView):
+class operator_list(ActiveListView):
     """
     View to list all operators and provide actions.
     """
     template_name = "core/operator_list.html"
     model = operator
-
-    def get_context_data(self, **kwargs):
-        context = super(operator_list, self).get_context_data(**kwargs)
-        context['active_list'] = operator.current.all()
-        context['inactive_list'] = operator.retired.all()
-        return context
