3d8f84bfc4d5116f422d11f29aeffa4d97daae63
Jonathan Marini <j.marini@ieee.org>
2014-07-29 10:59:42 -0400

    Added filtering functionality to the list/create view

--- afm/api.py
+++ afm/api.py
@@ -1,16 +1,18 @@
 from rest_framework import generics, permissions
 
+from .filters import AFMFilter
 from .models import afm
 from .serializers import AFMSerializer
 
 
-class AFMListAPI(generics.ListAPIView):
+class AFMListAPI(generics.ListCreateAPIView):
     """
     List all afm scans or create a new one via api.
     """
     queryset = afm.objects.all()
     serializer_class = AFMSerializer
     permission_classes = (permissions.IsAuthenticated, )
+    filter_class = AFMFilter
 
 
 class AFMDetailAPI(generics.RetrieveUpdateAPIView):
