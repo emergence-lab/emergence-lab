f7b235a83fadccd71bfa0e83e4dd14c6001ef086
Jonathan Marini <j.marini@ieee.org>
2014-08-27 18:04:58 -0400

    Added project tracking to admin
    Added ability to add projects to a user in the operator admin page.

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 import core.models
 
 
-admin.site.register(core.models.operator)
+class ProjectTrackingInline(admin.TabularInline):
+    model = core.models.project_tracking
+    extra = 1
+
+class OperatorAdmin(admin.ModelAdmin):
+    inlines = (ProjectTrackingInline, )
+    fields = ('name', 'active', 'user')
+
+
+admin.site.register(core.models.operator, OperatorAdmin)
 admin.site.register(core.models.platter)
 admin.site.register(core.models.project)
 admin.site.register(core.models.investigation)
+admin.site.register(core.models.project_tracking)
