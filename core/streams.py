f7f5bdc4e7f0c2f5b216426f14cfdf58d41f08f4
Jonathan Marini <j.marini@ieee.org>
2014-09-03 16:46:30 -0400

    Updated core.streams to reduce code duplication

--- core/streams.py
+++ core/streams.py
@@ -5,33 +5,23 @@ from actstream.models import actor_stream, target_stream
 from .models import operator, project, investigation
 
 
-def operator_project_stream(operator, project):
-    """
-    Filters the activity stream to return items the operator has done
-    for the specified project.
-    """
-    project_content_id = ContentType.objects.get(name='project').id
-    return actor_stream(operator).filter(target_content_type_id=project_content_id,
-                                         target_object_id=project.id)
+_operator_contentid = ContentType.objects.get(name='operator').id
 
 
 def project_stream(project):
     """
     Filters the activity stream to return any item for the specified project.
     """
-    project_content_id = ContentType.objects.get(name='project').id
     return target_stream(project)
 
 
-def operator_investigation_stream(operator, investigation):
+def operator_project_stream(operator, project):
     """
     Filters the activity stream to return items the operator has done
-    for the specified investigation.
+    for the specified project.
     """
-    project = investigation.project
-    stream = operator_project_stream(operator, project)
-    return [item for item in stream
-            if item.data.get('investigation', 0) == investigation.id]
+    return project_stream(project).filter(actor_content_type_id=_operator_contentid,
+                                          actor_object_id=operator.id)
 
 
 def investigation_stream(investigation):
@@ -42,4 +32,17 @@ def investigation_stream(investigation):
     project = investigation.project
     stream = project_stream(project)
     return [item for item in stream
-            if item.data.get('investigation', 0) == investigation.id]
+            if item.data is not None
+            and item.data.get('investigation', 0) == investigation.id]
+
+
+def operator_investigation_stream(operator, investigation):
+    """
+    Filters the activity stream to return items the operator has done
+    for the specified investigation.
+    """
+    project = investigation.project
+    stream = operator_project_stream(operator, project)
+    return [item for item in stream
+            if item.data is not None
+            and item.data.get('investigation', 0) == investigation.id]
