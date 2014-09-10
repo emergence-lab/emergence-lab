cce33245de5ed0efb5804f980f1b1ab0b23d652a
Jonathan Marini <j.marini@ieee.org>
2014-09-10 11:31:52 -0400

    Improve django admin display for growths and samples.

--- growths/admin.py
+++ growths/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+
 import growths.models
 
 
-admin.site.register(growths.models.growth)
-admin.site.register(growths.models.sample)
+class SampleAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'parent', 'size')
+    ordering = ('-growth', 'pocket', 'piece')
+
+
+class GrowthAdmin(admin.ModelAdmin):
+    list_display = ('growth_number', 'date', 'operator', 'project',
+                    'investigation', 'reactor')
+    ordering = ('-growth_number',)
+    list_filter = ('operator', 'project', 'investigation', 'reactor')
+
+
+admin.site.register(growths.models.growth, GrowthAdmin)
+admin.site.register(growths.models.sample, SampleAdmin)
