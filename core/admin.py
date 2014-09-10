4bea4f022e1151d7aafb2f23e58b32649115fc11
Jonathan Marini <j.marini@ieee.org>
2014-09-10 11:45:26 -0400

    Improve admin list for projects/investigations

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+
 import core.models
+
+
+class InvestigationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'project', 'active', 'start_date')
+    list_filter = ('project', 'active')
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'active', 'start_date')
+    list_filter = ('active',)
 
 
 admin.site.register(core.models.operator)
 admin.site.register(core.models.platter)
-admin.site.register(core.models.project)
-admin.site.register(core.models.investigation)
+admin.site.register(core.models.project, ProjectAdmin)
+admin.site.register(core.models.investigation, InvestigationAdmin)
