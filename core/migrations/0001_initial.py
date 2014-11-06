9a55dea706081ae5c9df24509d5510b57ac3f193
Jonathan Marini <j.marini@ieee.org>
2014-11-06 11:20:05 -0500

    Removed old migrations.
    Data will be migrated over externally due to major changes in data
    layout.

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -4,28 +4,57 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import autoslug.fields
 import ckeditor.fields
+import django.utils.timezone
 from django.conf import settings
+import django.core.validators
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='investigation',
+            name='User',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=45)),
-                ('slug', autoslug.fields.AutoSlugField(editable=False)),
-                ('active', models.BooleanField(default=True)),
-                ('description', ckeditor.fields.RichTextField(blank=True)),
-                ('start_date', models.DateTimeField(auto_now_add=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
+                ('username', models.CharField(help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, max_length=30, verbose_name='username', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')])),
+                ('full_name', models.CharField(max_length=255, verbose_name='full name', blank=True)),
+                ('short_name', models.CharField(max_length=50, verbose_name='preferred name', blank=True)),
+                ('email', models.EmailField(max_length=75, verbose_name='email address', blank=True)),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('groups', models.ManyToManyField(related_query_name='custom_user', related_name='custom_users', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups')),
+            ],
+            options={
+                'db_table': 'users',
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Investigation',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('is_active', models.BooleanField(default=True, verbose_name='active')),
+                ('status_changed', models.DateTimeField(verbose_name='status changed', null=True, editable=False, blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name='date created')),
+                ('modified', models.DateTimeField(auto_now=True, verbose_name='date modified')),
+                ('name', models.CharField(max_length=45, verbose_name='name')),
+                ('slug', autoslug.fields.AutoSlugField(verbose_name='slug', editable=False)),
+                ('description', ckeditor.fields.RichTextField(verbose_name='description', blank=True)),
             ],
             options={
                 'db_table': 'investigations',
+                'verbose_name': 'investigation',
+                'verbose_name_plural': 'investigations',
             },
             bases=(models.Model,),
         ),
@@ -33,42 +62,34 @@ class Migration(migrations.Migration):
             name='operator',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('is_active', models.BooleanField(default=True, verbose_name='active')),
+                ('status_changed', models.DateTimeField(verbose_name='status changed', null=True, editable=False, blank=True)),
                 ('name', models.CharField(max_length=45)),
-                ('active', models.BooleanField(default=True)),
             ],
             options={
                 'db_table': 'operators',
+                'verbose_name': 'operator',
+                'verbose_name_plural': 'operators',
             },
             bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='platter',
+            name='Project',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=45)),
-                ('active', models.BooleanField(default=True)),
-                ('serial', models.CharField(max_length=20, blank=True)),
-                ('start_date', models.DateField(auto_now_add=True)),
-                ('end_date', models.DateField(null=True, blank=True)),
-            ],
-            options={
-                'db_table': 'platters',
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='project',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=45)),
-                ('slug', autoslug.fields.AutoSlugField(editable=False)),
-                ('active', models.BooleanField(default=True)),
-                ('core', models.BooleanField(default=False)),
-                ('description', ckeditor.fields.RichTextField(blank=True)),
-                ('start_date', models.DateTimeField(auto_now_add=True)),
+                ('is_active', models.BooleanField(default=True, verbose_name='active')),
+                ('status_changed', models.DateTimeField(verbose_name='status changed', null=True, editable=False, blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name='date created')),
+                ('modified', models.DateTimeField(auto_now=True, verbose_name='date modified')),
+                ('name', models.CharField(max_length=45, verbose_name='name')),
+                ('slug', autoslug.fields.AutoSlugField(verbose_name='slug', editable=False)),
+                ('core', models.BooleanField(default=False, verbose_name='core project')),
+                ('description', ckeditor.fields.RichTextField(verbose_name='description', blank=True)),
             ],
             options={
                 'db_table': 'projects',
+                'verbose_name': 'project',
+                'verbose_name_plural': 'projects',
             },
             bases=(models.Model,),
         ),
@@ -78,17 +99,30 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('is_pi', models.BooleanField(default=False)),
                 ('operator', models.ForeignKey(to='core.operator')),
-                ('project', models.ForeignKey(to='core.project')),
+                ('project', models.ForeignKey(to='core.Project')),
             ],
             options={
                 'db_table': 'project_operator_tracking',
             },
             bases=(models.Model,),
         ),
+        migrations.CreateModel(
+            name='ProjectTracking',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('is_owner', models.BooleanField(default=False)),
+                ('project', models.ForeignKey(to='core.Project')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'db_table': 'project_tracking',
+            },
+            bases=(models.Model,),
+        ),
         migrations.AddField(
             model_name='operator',
             name='projects',
-            field=models.ManyToManyField(to='core.project', through='core.project_tracking'),
+            field=models.ManyToManyField(to='core.Project', through='core.project_tracking'),
             preserve_default=True,
         ),
         migrations.AddField(
@@ -100,7 +134,19 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='investigation',
             name='project',
-            field=models.ForeignKey(to='core.project'),
+            field=models.ForeignKey(verbose_name='project', to='core.Project'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='projects',
+            field=models.ManyToManyField(related_query_name='user', related_name='users', to='core.Project', through='core.ProjectTracking', blank=True, help_text='Projects this user is tracking', verbose_name='tracked projects'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='user_permissions',
+            field=models.ManyToManyField(related_query_name='custom_user', related_name='custom_users', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions'),
             preserve_default=True,
         ),
     ]
