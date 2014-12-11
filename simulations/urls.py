82d808ca8391fb5196764ac89590ebe1dcfcdefc
Neil Newman <nnewman2@albany.edu>
2014-12-11 16:16:27 -0500

    Added management view

--- simulations/urls.py
+++ simulations/urls.py
@@ -3,10 +3,14 @@ from simulations import views
 from simulations import models
 from django.views.decorators.cache import never_cache
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = patterns('',
     url(r'^new/$', login_required(views.SimulationCreate.as_view()), name='create_form'),
     url(r'^edit/(?P<pk>\d+)/$', login_required(never_cache(views.SimulationEdit.as_view())), name='simulation_edit'),
     url(r'^cancel/(?P<pk>\d+)/$', login_required(never_cache(views.SimulationCancel.as_view())), name='simulation_cancel'),
     url(r'^$', login_required(views.SimulationLanding.as_view()), name='simulation_landing'),
+    url(r'^management$', staff_member_required(views.SimulationManagement.as_view()), name='simulation_management'),
+    url(r'^start_instance/(?P<instance_id>[a-z0-9\-]+)$', staff_member_required(views.StartInstance.as_view()), name='start_instance'),
+    url(r'^stop_instance/(?P<instance_id>[a-z0-9\-]+)$', staff_member_required(views.StopInstance.as_view()), name='stop_instance'),
 )
--- simulations/views.py
+++ simulations/views.py
@@ -68,4 +68,51 @@ class SimulationEdit(UpdateView):
     template_name = 'simulations/edit_form.html'
     success_url = '/simulations'
 
+class SimulationManagement(ListView):
+    model = Simulation
+    queryset = Simulation.objects.all()
+    template_name = 'simulations/management.html'
     
+    signals = metal.EC2_Connection(settings.AWS_EC2_REGION, settings.AWS_EC2_KEY, settings.AWS_EC2_SECRET)
+    
+    def get_context_data(self, **kwargs):
+        tmp = []
+        if 'instances' not in kwargs:
+            for instance in self.signals.reservation_list()[0].instances:
+                tmp.append({'instance_name': instance.id,
+                            'instance_type': instance.instance_type,
+                            'instance_state': instance.state})
+            kwargs['instances'] = tmp
+        if 'status' not in kwargs:
+            kwargs['status'] = self.signals.instance_list()[0].state
+        return super(SimulationManagement, self).get_context_data(**kwargs)
+    
+    def get_queryset(self):
+        return Simulation.objects.exclude(completed=True).order_by('id')
+    
+    def index(request):
+        return HttpResponse(queryset)
+   
+class StartInstance(RedirectView):
+    permanent = False
+
+    def get_redirect_url(self, *args, **kwargs):
+        signals = metal.EC2_Connection(settings.AWS_EC2_REGION, settings.AWS_EC2_KEY, settings.AWS_EC2_SECRET)
+        instance = kwargs.pop('instance_id')
+        try:
+            signals.start_instance(instance)
+            return reverse('simulation_management')
+        except Exception as e:
+            raise Exception(e)
+    
+class StopInstance(RedirectView):
+    permanent = False
+    
+    def get_redirect_url(self, *args, **kwargs):
+        signals = metal.EC2_Connection(settings.AWS_EC2_REGION, settings.AWS_EC2_KEY, settings.AWS_EC2_SECRET)
+        instance = kwargs.pop('instance_id')
+        try:
+            signals.stop_instance(instance)
+            return reverse('simulation_management')
+        except Exception as e:
+            raise Exception(e)
