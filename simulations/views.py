8e2898d7e35b465abfccb4a706c84785ceac6c45
Neil Newman <nnewman2@albany.edu>
2014-12-17 11:01:54 -0500

    Cleaned up unused views

--- simulations/views.py
+++ simulations/views.py
@@ -80,36 +80,6 @@ class SimulationEdit(UpdateView):
     fields = ['priority', 'execution_node', 'file_path']
     template_name = 'simulations/edit_form.html'
     success_url = '/simulations'
-
-class SimulationManagement(ListView):
-    model = Simulation
-    queryset = Simulation.objects.all()
-    template_name = 'simulations/management.html'
-    
-    signals = metal.EC2_Connection(settings.AWS_EC2_REGION, settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
-    
-    def get_context_data(self, **kwargs):
-        tmp = []
-        if 'instances' not in kwargs:
-            for instance in self.signals.reservation_list()[0].instances:
-                tmp.append({'instance_name': instance.id,
-                            'instance_type': instance.instance_type,
-                            'instance_state': instance.state,
-                            #'instance_launchtime': datetime.datetime.fromtimestamp(\
-                            #    time.mktime(time.strptime(instance.launch_time.split('.')[0],
-                            #                              "%Y-%m-%dT%H:%M:%S")))
-                            'instance_uptime': self.signals.instance_uptime(instance.id)})
-            kwargs['instances'] = tmp
-        if 'status' not in kwargs:
-            kwargs['status'] = self.signals.instance_list()[0].state
-        return super(SimulationManagement, self).get_context_data(**kwargs)
-    
-    def get_queryset(self):
-        #return Simulation.objects.exclude(completed=True).order_by('id')
-        return Simulation.objects.order_by('-id')
-    
-    def index(request):
-        return HttpResponse(queryset)
    
 class StartInstance(RedirectView):
     permanent = False
