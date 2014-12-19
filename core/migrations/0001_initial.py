b6f41cd0b326fe435cdb56de4595c61a1d7e451f
Jonathan Marini <j.marini@ieee.org>
2014-12-19 14:52:11 -0500

    Cleaned up core migrations.

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -172,7 +172,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='processnode',
             name='process',
-            field=models.ForeignKey(to='core.Process'),
+            field=models.ForeignKey(to='core.Process', null=True),
             preserve_default=True,
         ),
         migrations.AddField(
