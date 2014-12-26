b6de4550ca2adc01d2c503bd1522adcae7506d28
Neil Newman <nnewman2@albany.edu>
2014-12-26 17:42:10 -0500

    Added ability to create from existing

--- simulations/views.py
+++ simulations/views.py
@@ -14,6 +14,7 @@ import time
 import zipfile
 import tempfile
 import json
+import shutil
 
 import aws_support as aws
 
@@ -68,17 +69,45 @@ class SimulationCreate(CreateView):
         #try:
         #    template_name = self.kwargs['template_name']
         #except KeyError: template_name = None
-        try:
-            base_path = os.path.join(settings.MEDIA_ROOT, 'simulations', 'templates', str(self.request.user), self.kwargs['template_name'])
-        except KeyError:
-            base_path = os.path.join(settings.MEDIA_ROOT, 'simulations', 'templates', 'global', 'default')
 
-        initial['materials'] = open(os.path.join(base_path,
-                                                 'materials.par')).read()
-        initial['physics'] = open(os.path.join(base_path,
-                                                 'physics.cmd')).read()
-        initial['device'] = open(os.path.join(base_path,
-                                                 'device.scm')).read()
+
+        try:
+            s3_file_path = Simulation.objects.get(pk=self.kwargs['pk']).file_path.name
+            storage = aws.S3FileManager(settings.AWS_ACCESS_KEY_ID,
+                                        settings.AWS_SECRET_ACCESS_KEY,
+                                        True)
+            zipdir = tempfile.mkdtemp()
+            os.mkdir(os.path.join(zipdir, 'simulations'))
+            zip_download = storage.downloadFileFromBucket(settings.AWS_STORAGE_BUCKET_NAME,
+                                                               s3_file_path,
+                                                               zipdir)
+            original_file = zipfile.ZipFile(os.path.join(zipdir,
+                                                         s3_file_path),
+                                            'r')
+            file_list = original_file.namelist()
+            for item in file_list:
+                if item.endswith('.par'): initial['materials'] = original_file.read(item)
+                if item.endswith('.scm'): initial['device'] = original_file.read(item)
+                if item.endswith('.cmd'): initial['physics'] = original_file.read(item)
+        except KeyError:
+            try:
+                base_path = os.path.join(settings.MEDIA_ROOT,
+                                         'simulations',
+                                         'templates',
+                                         str(self.request.user),
+                                         self.kwargs['template_name'])
+            except KeyError:
+                base_path = os.path.join(settings.MEDIA_ROOT,
+                                         'simulations',
+                                         'templates',
+                                         'global',
+                                         'default')
+            initial['materials'] = open(os.path.join(base_path,
+                                                     'materials.par')).read()
+            initial['physics'] = open(os.path.join(base_path,
+                                                     'physics.cmd')).read()
+            initial['device'] = open(os.path.join(base_path,
+                                                     'device.scm')).read()
         return initial
 
     def form_valid(self, form):
@@ -151,10 +180,12 @@ class SimulationCancel(RedirectView):
 class SimulationEdit(UpdateView):
     model = Simulation
     form_class = SimInlineForm
-    template_name = 'simulations/create_form.html'
+    template_name = 'simulations/edit_form.html'
     success_url = '/simulations'
 
-    storage = aws.S3FileManager(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY, True)
+    storage = aws.S3FileManager(settings.AWS_ACCESS_KEY_ID,
+                                settings.AWS_SECRET_ACCESS_KEY,
+                                True)
     zipdir = tempfile.mkdtemp()
     os.mkdir(os.path.join(zipdir, 'simulations'))
 
@@ -163,30 +194,17 @@ class SimulationEdit(UpdateView):
         Returns the initial data to use for forms on this view.
         """
         initial = super(SimulationEdit, self).get_initial()
-        #try:
-        #    template_name = self.kwargs['template_name']
-        #except KeyError: template_name = None
-        zip_download = self.storage.downloadFileFromBucket(settings.AWS_STORAGE_BUCKET_NAME, self.object.file_path.name, self.zipdir)
-        original_file = zipfile.ZipFile(os.path.join(self.zipdir, self.object.file_path.name), 'r')
+        zip_download = self.storage.downloadFileFromBucket(settings.AWS_STORAGE_BUCKET_NAME,
+                                                           self.object.file_path.name,
+                                                           self.zipdir)
+        original_file = zipfile.ZipFile(os.path.join(self.zipdir,
+                                                     self.object.file_path.name),
+                                        'r')
         self.file_list = original_file.namelist()
         for item in self.file_list:
             if item.endswith('.par'): initial['materials'] = original_file.read(item)
             if item.endswith('.scm'): initial['device'] = original_file.read(item)
             if item.endswith('.cmd'): initial['physics'] = original_file.read(item)
-        #try:
-        #    materials_path = file_list['par']
-        #    device_path = file_list['scm']
-        #    physics_path = file_list['cmd']
-        #except Exception as e: print(e)
-
-        #try:
-        #    base_path = os.path.join(settings.MEDIA_ROOT, 'simulations', 'templates', str(self.request.user), self.kwargs['template_name'])
-        #except KeyError:
-        #    base_path = os.path.join(settings.MEDIA_ROOT, 'simulations', 'templates', 'global', 'default')
-
-        #initial['materials'] = open(materials_path).read()
-        #initial['physics'] = open(physics_path).read()
-        #initial['device'] = open(device_path).read()
         return initial
 
     def form_valid(self, form):
@@ -300,3 +318,42 @@ class SimulationTemplates(TemplateView):
                                     'comment': comment})
                 kwargs['template_list'] = tmp
         return super(SimulationTemplates, self).get_context_data(**kwargs)
+
+#class SimulationTemplateCopy(RedirectView):
+#    permanent = False
+#
+#    def get_redirect_url(self, *args, **kwargs):
+#        signals = aws.EC2Connection(settings.AWS_EC2_REGION, settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+#        instance = kwargs.pop('instance_id')
+#        try:
+#            signals.stop_instance(instance)
+#            return reverse('simulation_admin')
+#        except Exception as e:
+#            raise Exception(e)
+
+class SimulationTemplateDelete(RedirectView):
+    permanent = False
+
+    def get_redirect_url(self, *args, **kwargs):
+        template_name = kwargs.pop('template_name')
+        user = str(self.request.user)
+        try:
+            for item in os.listdir(os.path.join(settings.MEDIA_ROOT, 'simulations', 'templates', user, template_name)):
+                os.remove(os.path.join(settings.MEDIA_ROOT, 'simulations', 'templates', user, template_name, item))
+            os.rmdir(os.path.join(settings.MEDIA_ROOT, 'simulations', 'templates', user, template_name))
+            return reverse('simulation_templates')
+        except Exception as e:
+            raise Exception(e)
+
+#class SimulationTemplateCreate(RedirectView):
+#    #pass
+#    permanent = False
+#
+#    def get_redirect_url(self, *args, **kwargs):
+#        storage = aws.S3FileManager(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY, True)
+#        instance = kwargs.pop('instance_id')
+#        try:
+#            signals.stop_instance(instance)
+#            return reverse('simulation_admin')
+#        except Exception as e:
+#            raise Exception(e)
