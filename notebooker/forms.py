422f72053f12862a2fb8183ca12304e8ca15f5c7
Neil Newman <nnewman2@albany.edu>
2015-01-03 16:52:50 -0500

    Added interactive form to update nb cells

--- notebooker/forms.py
+++ notebooker/forms.py
@@ -3,11 +3,32 @@ from django.forms import ModelForm
 from django_ace import AceWidget
 
 class NBCellEdit(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.cell_count = kwargs.pop('cell_count', None)
+        #super(NBCellEdit, self).__init__(*args, **kwargs)
+        super(NBCellEdit, self).__init__(*args, **kwargs)
 
-    cell = forms.CharField(widget=AceWidget(mode='text',
-                                                #theme='twilight',
-                                                width="800px",
-                                                height="300px",
-                                                showprintmargin=True))
+        #self.fields['cell'] = []
+        #self.fields['cell'] = {}
+        for i in range(self.cell_count):
+            self.fields['cell_{}'.format(i)] = forms.CharField(widget=AceWidget(mode='text',
+                                                        #theme='twilight',
+                                                        width="400px",
+                                                        height="100px",
+                                                        #label="LABEL",
+                                                        showprintmargin=True),
+                                                     required=False)
+            #self.fields['cell'][i].label = 'LABEL'
+        #super(NBCellEdit, self).__init__(*args, **kwargs)
+    #cell_upd = forms.IntegerField(required=False)
+    #comment = forms.CharField(required=False, max_length=200)
 
-    comment = forms.CharField(required=False, max_length=200)
+#class NBCellEdit(forms.Form):
+#
+#    cell = forms.CharField(widget=AceWidget(mode='text',
+#                                                #theme='twilight',
+#                                                width="800px",
+#                                                height="300px",
+#                                                showprintmargin=True))
+#
+#    comment = forms.CharField(required=False, max_length=200)
--- notebooker/views.py
+++ notebooker/views.py
@@ -64,22 +64,48 @@ class NotebookDemo(TemplateView):
             kwargs['comments'] = tmp
         return super(NotebookDemo, self).get_context_data(**kwargs)
 
-class NotebookIntDemo(TemplateView):
-    template_name = 'notebooker/demo_int_nb.html'
+class NotebookIntDemo(FormView):
+    template_name = 'notebooker/demo_interactive_nb.html'
+    form_class = NBCellEdit
+
+    def get_form_kwargs(self):
+        kwargs = super(NotebookIntDemo, self).get_form_kwargs()
+        user = str(self.request.user)
+        if 'cell_count' not in kwargs:
+            nb = json.load(open(os.path.join(settings.MEDIA_ROOT, 'notebooks', user, str(self.kwargs['notebook_name'] + '.ipynb')), 'r'))
+            kwargs['cell_count'] = int(len(nb['worksheets'][0]['cells']))
+        return kwargs
+
+    def get_initial(self):
+        initial = super(NotebookIntDemo, self).get_initial()
+        #cell_num = int(self.kwargs['cell'])
+        user = str(self.request.user)
+        nb = json.load(open(os.path.join(settings.MEDIA_ROOT, 'notebooks', user, str(self.kwargs['notebook_name'] + '.ipynb')), 'r'))
+        for cell_num, i in enumerate(nb['worksheets'][0]['cells']):
+            tmp = []
+            if nb['worksheets'][0]['cells'][cell_num]['cell_type'] == 'code':
+                tmp = nb['worksheets'][0]['cells'][cell_num]['input']
+            elif nb['worksheets'][0]['cells'][cell_num]['cell_type'] == 'markdown':
+                tmp = nb['worksheets'][0]['cells'][cell_num]['source']
+            #initial['cell'] = nb['worksheets'][0]['cells'][cell_num]['input']
+            with open(tempfile.NamedTemporaryFile().name, 'wb+') as output:
+                for i in tmp:
+                    output.write(i)
+                output.flush()
+                output.seek(0)
+                initial['cell_{}'.format(cell_num)] = output.read()
+                output.close
+        return initial
 
     def get_context_data(self, **kwargs):
-        filename = kwargs['notebook_name']
+        filename = self.kwargs['notebook_name']
         user = str(self.request.user)
-
         e = HTMLExporter(config=Config({'HTMLExporter':{'default_template':'basic'}}))
         path = os.path.join(settings.MEDIA_ROOT, 'notebooks', user, str(filename + '.ipynb'))
         with open(path, 'r') as nb:
             notebook = json.loads(nb.read())
         if 'notebook_content' not in kwargs:
             cntnt = []
-            cleaned_cntnt = []
-            #kwargs['notebook_content'] = str(e.from_filename(path))
-
             for cell in notebook['worksheets'][0]['cells']:
                 with open(tempfile.NamedTemporaryFile().name, 'wb+') as output:
                     m = {}
@@ -94,10 +120,6 @@ class NotebookIntDemo(TemplateView):
                     p['worksheets'] = c
                     output.write(json.dumps(p))
                     output.seek(0)
-                    #cntnt.append(e.from_file(output))
-                    #for i in e.from_file(output):
-                    #    if 'defaultdict' not in i:
-                    #    cntnt.append(i)
                     with open (tempfile.NamedTemporaryFile().name, 'wb+') as cleaned_output:
                         for i in e.from_file(output):
                             if 'defaultdict' not in str(i):
@@ -106,32 +128,112 @@ class NotebookIntDemo(TemplateView):
                         cleaned_output.seek(0)
                         cntnt.append(cleaned_output.read())
                         cleaned_output.close()
-
                     output.close()
-            #for i in cntnt:
-            #    if not str(i).startswith('defaultdict'):
-            #        cleaned_cntnt.append(i)
-
-        #        with open(tempfile.NamedTemporaryFile().name, 'wb+') as output:
-        #        #with open(os.path.join(settings.MEDIA_ROOT, 'notebooks', user, 'out1.html'), 'wb') as output:
-        #
-        #            for i in e.from_filename(path):
-        #                if not str(i).startswith('defaultdict'):
-        #                    output.write(i)
-        #            output.flush()
-        #            output.seek(0)
-        #            kwargs['notebook_content'] = output.read()
-        #            output.close()
             kwargs['notebook_content'] = cntnt
         if 'comments' not in kwargs:
-
             tmp = []
             with open(os.path.join(settings.MEDIA_ROOT, 'notebooks', user, 'test.json'), 'r') as comment:
                 comments = json.load(comment)
                 for i in comments:
                     tmp.append(comments[i])
             kwargs['comments'] = tmp
+        if 'cell_count' not in kwargs:
+            nb = json.load(open(os.path.join(settings.MEDIA_ROOT, 'notebooks', user, str(self.kwargs['notebook_name'] + '.ipynb')), 'r'))
+            kwargs['cell_count'] = range(int(len(nb['worksheets'][0]['cells'])))
+        if 'notebook_name' not in kwargs:
+            kwargs['notebook_name'] = 'Untitled0'
         return super(NotebookIntDemo, self).get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = str(self.request.user)
+        #cell_num = int(form.cleaned_data['Submit'])
+
+        #self.kwargs['notebook_name'] = 'Untitled0'
+        filename = str(self.kwargs['notebook_name'] + '.ipynb')
+        #cell_num = 0
+        #cell_num = int(self.kwargs['cell'])
+        nb_path = os.path.join(settings.MEDIA_ROOT, 'notebooks', user, filename)
+        try:
+            nb = json.load(open(nb_path, 'r'))
+
+            cell_count = len(nb['worksheets'][0]['cells'])
+
+            for i in range(cell_count):
+                if 'upd_cell_{}'.format(int(i + 1)) in self.request.POST:
+                    cell_num = int(i)
+                elif 'upd_all' in self.request.POST:
+                    pass
+
+            if nb['worksheets'][0]['cells'][cell_num]['cell_type'] == 'code':
+                nb['worksheets'][0]['cells'][cell_num]['input'] = form.cleaned_data['cell_{}'.format(cell_num)]
+            elif nb['worksheets'][0]['cells'][cell_num]['cell_type'] == 'markdown':
+                nb['worksheets'][0]['cells'][cell_num]['source'] = form.cleaned_data['cell_{}'.format(cell_num)]
+            with open(nb_path, 'wb+') as output:
+                try:
+                    output.truncate()
+                    output.seek(0)
+                    json.dump(nb, output)
+                    output.close()
+                except Exception as e: print(e)
+            nb = read(open(nb_path), 'json')
+            r = NotebookRunner(nb)
+            r.run_notebook(skip_exceptions=True)
+            #for i, cell in enumerate(r.iter_code_cells()):
+            #    if i == cell_num:
+            #        try:
+            #            r.run_cell(cell, skip_exceptions=True)
+            #        except NotebookError:
+            #            if not skip_exceptions:
+            #                raise
+            write(r.nb, open(nb_path, 'w'), 'json')
+        except Exception as e: print(e)
+
+        return HttpResponseRedirect(reverse('notebook_int_demo', kwargs = {'notebook_name': self.kwargs['notebook_name']}))
+
+#class NotebookIntDemo(TemplateView):
+#    template_name = 'notebooker/demo_int_nb.html'
+#
+#    def get_context_data(self, **kwargs):
+#        filename = kwargs['notebook_name']
+#        user = str(self.request.user)
+#        e = HTMLExporter(config=Config({'HTMLExporter':{'default_template':'basic'}}))
+#        path = os.path.join(settings.MEDIA_ROOT, 'notebooks', user, str(filename + '.ipynb'))
+#        with open(path, 'r') as nb:
+#            notebook = json.loads(nb.read())
+#        if 'notebook_content' not in kwargs:
+#            cntnt = []
+#            for cell in notebook['worksheets'][0]['cells']:
+#                with open(tempfile.NamedTemporaryFile().name, 'wb+') as output:
+#                    m = {}
+#                    t = []
+#                    t.append(cell)
+#                    m['cells'] = t
+#                    c = []
+#                    c.append(m)
+#                    p = {}
+#                    p['metadata'] = {}
+#                    p['nbformat'] = 3
+#                    p['worksheets'] = c
+#                    output.write(json.dumps(p))
+#                    output.seek(0)
+#                    with open (tempfile.NamedTemporaryFile().name, 'wb+') as cleaned_output:
+#                        for i in e.from_file(output):
+#                            if 'defaultdict' not in str(i):
+#                                cleaned_output.write(i)
+#                        cleaned_output.flush()
+#                        cleaned_output.seek(0)
+#                        cntnt.append(cleaned_output.read())
+#                        cleaned_output.close()
+#                    output.close()
+#            kwargs['notebook_content'] = cntnt
+#        if 'comments' not in kwargs:
+#            tmp = []
+#            with open(os.path.join(settings.MEDIA_ROOT, 'notebooks', user, 'test.json'), 'r') as comment:
+#                comments = json.load(comment)
+#                for i in comments:
+#                    tmp.append(comments[i])
+#            kwargs['comments'] = tmp
+#        return super(NotebookIntDemo, self).get_context_data(**kwargs)
 
 class CellEdit(FormView):
     template_name = 'notebooker/cell_edit_form.html'
