4a0d55912bca4b5bc44e168effbf899ccb5fb446
Jonathan Marini <j.marini@ieee.org>
2015-01-13 11:05:53 -0500

    Cleaned up navbar js slightly. Still needs work.

--- core/static/core/js/nav_search_helper.js
+++ core/static/core/js/nav_search_helper.js
@@ -1,42 +1,22 @@
 // When your page loads
 $(document).ready(function(){
-   // When the toggle areas in your navbar are clicked, toggle them
-   $("#search-form").hide();
-   $("#search-close").hide();
+  $('.navbar-search').hide();
 
+  $('#search-button').click(function(e) {
+    $('.navbar-search').show();
+    $('#search-entry').focus();
+    $('.navbar-button').hide();
+  });
 
-   $("#search-button").click(function(e){
-       e.preventDefault();
-       $("#search-form").toggle();
-       $("#search-close").toggle();
-       $("#search-entry").focus();
-       $("#homelink").toggle();
-       $("#create-button").toggle();
-       $("#apps-dropdown").toggle();
-       $("#notifications").toggle();
+  $('#search-entry').focusout(function(e) {
+    $('.navbar-search').hide();
+    $('.navbar-button').show();
+  });
 
-       $("#search-button").toggle();
+  $('#search-close').click(function(e) {
+    e.preventDefault();
+    $('.navbar-button').show();
+    $('.navbar-search').hide();
+  });
 
-       $("#search-form").focusout(function(f){
-         $("#search-form").toggle();
-         $("#search-entry").focus();
-         $("#homelink").toggle();
-         $("#create-button").toggle();
-         $("#apps-dropdown").toggle();
-         $("#search-close").toggle();
-         $("#notifications").toggle();
-
-         $("#search-button").toggle();
-       });
-
-   });
-   $("#search-close").click(function(g){
-      $("#search-form").hide();
-      $("#homelink").toggle();
-      $("#create-button").toggle();
-      $("#apps-dropdown").toggle();
-      $("#search-close").hide();
-      $("#notifications").toggle();
-      $("#search-button").toggle();
-    });
-})
+});
