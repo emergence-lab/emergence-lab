a7de78f7c7f32962531ee56d96df91e3b9643569
Jonathan Marini <j.marini@ieee.org>
2015-01-16 13:49:18 -0500

    Moved context processor to messaging app.

--- core/context_processors.py
+++
@@ -1,8 +0,0 @@
-### Context processors added to the base of all templates
-
-def notifications(request):
-    from django.conf import settings
-    from messaging.redis_config import Helper
-    helper = Helper()
-    notification_objects = helper.get_notifications(request.user.id)
-    return {'notifications': notification_objects}
---
+++ messaging/context_processors.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.conf import settings
+
+from .redis_config import Helper
+
+
+def notifications(request):
+    """
+    Add notifications to context for all views.
+    """
+    helper = Helper()
+    notification_objects = helper.get_notifications(request.user.id)
+    return {'notifications': notification_objects}
