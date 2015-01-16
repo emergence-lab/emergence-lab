1830f7c8528289536b8204783774768b87076b9f
Jonathan Marini <j.marini@ieee.org>
2015-01-16 16:53:09 -0500

    Changed track project to only use active projects.

--- core/forms/project.py
+++ core/forms/project.py
@@ -3,10 +3,11 @@ from __future__ import absolute_import, unicode_literals
 
 from django import forms
 
-from core.models import ProjectTracking
+from core.models import Project, ProjectTracking
 
 
 class TrackProjectForm(forms.ModelForm):
+    project = forms.ModelChoiceField(queryset=Project.active.all())
 
     def save(self, **kwargs):
         commit = kwargs.pop('commit', True)
