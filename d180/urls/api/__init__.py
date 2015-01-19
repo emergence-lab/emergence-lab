d1c685d3f5c13a26f87f513feda54fc3ff48c507
Neil Newman <nnewman2@albany.edu>
2015-01-19 13:51:12 -0500

    Added in ReadingsDetail and ReadingsCreate API views

--- d180/urls/api/__init__.py
+++ d180/urls/api/__init__.py
@@ -8,4 +8,6 @@ from d180 import api
 
 urlpatterns = [
     url(r'^readings/$', api.D180ReadingsListAPI.as_view()),
+    url(r'^readings/(?P<pk>\d+)/$', api.D180ReadingsDetailAPI.as_view()),
+    url(r'^readings/create/$', api.D180ReadingsCreateAPI.as_view()),
 ]
