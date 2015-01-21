bde6fba827ff7d61fc6cc88bf3954356433e5e6c
Jonathan Marini <j.marini@ieee.org>
2015-01-21 10:09:47 -0500

    Changed core.models.mixins tests to use test-only models.

--- core/tests/migrations/0001_initial.py
+++ core/tests/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import core.models.fields
 
 
 class Migration(migrations.Migration):
@@ -12,9 +13,31 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='ActiveStateModel',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('is_active', models.BooleanField(default=True, verbose_name='active')),
+                ('status_changed', models.DateTimeField(editable=False, null=True, verbose_name='status changed', blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='AutoUUIDModel',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='ParentProcess',
             fields=[
-                ('process_ptr', models.OneToOneField(serialize=False, auto_created=True, primary_key=True, parent_link=True, to='core.Process')),
+                ('process_ptr', models.OneToOneField(auto_created=True, serialize=False, primary_key=True, parent_link=True, to='core.Process')),
                 ('parent_field', models.IntegerField()),
             ],
             options={
@@ -25,12 +48,23 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ChildProcess',
             fields=[
-                ('parentprocess_ptr', models.OneToOneField(serialize=False, auto_created=True, primary_key=True, parent_link=True, to='tests.ParentProcess')),
+                ('parentprocess_ptr', models.OneToOneField(auto_created=True, serialize=False, primary_key=True, parent_link=True, to='tests.ParentProcess')),
                 ('child_field', models.IntegerField()),
             ],
             options={
                 'abstract': False,
             },
             bases=('tests.parentprocess',),
+        ),
+        migrations.CreateModel(
+            name='UUIDModel',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('uuid_full', core.models.fields.UUIDField(max_length=32, blank=True, editable=False, unique=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
         ),
     ]
