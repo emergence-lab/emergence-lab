0db147c2e3d9ec85bd04771d9f9a2ef627d935b5
Jonathan Marini <j.marini@ieee.org>
2015-01-27 16:40:37 -0500

    Added initial growth wizard post tests.

--- d180/tests/test_views.py
+++ d180/tests/test_views.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import resolve, reverse
+from django.forms import ValidationError
 from django.test import TestCase
 
 from model_mommy import mommy
@@ -114,3 +115,36 @@ class TestD180Wizard(TestCase):
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'growths/create_growth_readings.html')
         self.assertEqual(response.status_code, 200)
+
+    def test_start_empty_data(self):
+        """
+        Test a post where no data is sent.
+        """
+        url = '/d180/growth/create/start/'
+        with self.assertRaises(ValidationError) as cm:
+            self.client.post(url, {})
+        exception = cm.exception
+        self.assertEqual(exception.message,
+            'ManagementForm data is missing or has been tampered with')
+
+    def test_start_management_only(self):
+        """
+        Test a post where only managementform data is passed.
+        """
+        url = '/d180/growth/create/start/'
+        data = {
+            'sample-INITIAL_FORMS': '0',
+            'sample-MAX_NUM_FORMS': '',
+            'sample-TOTAL_FORMS': '1',
+        }
+        response = self.client.post(url, data)
+        self.assertFormError(response, 'info_form', 'user',
+            'This field is required.')
+        self.assertFormError(response, 'growth_form', None,
+            'At least one material must be specified')
+        self.assertFormError(response, 'checklist_form', 'field_0',
+            'This field is required.')
+        self.assertFormError(response, 'source_form', 'cp2mg',
+            'This field is required.')
+        self.assertFormsetError(response, 'sample_formset', 0, None,
+            'Cannot leave all fields blank.')
