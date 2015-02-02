2758c4c0af29226a28f5b1c964d1fa46b523ac1b
Jonathan Marini <j.marini@ieee.org>
2015-02-02 18:05:20 -0500

    Added tests for remaining growth wizard views.

--- d180/tests/test_views.py
+++ d180/tests/test_views.py
@@ -103,6 +103,15 @@ class TestD180Wizard(TestCase):
         self.assertTemplateUsed(response, 'growths/create_growth_readings.html')
         self.assertEqual(response.status_code, 200)
 
+    def test_postrun_resolution_template(self):
+        mommy.make(D180Growth)
+        url = '/d180/growth/create/postrun/'
+        match = resolve(url)
+        self.assertEqual(match.url_name, 'create_growth_d180_postrun')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'growths/create_growth_postrun.html')
+        self.assertEqual(response.status_code, 200)
+
     def test_start_empty_data(self):
         """
         Test a post where no data is sent.
@@ -227,3 +236,138 @@ class TestD180Wizard(TestCase):
         }
         response = self.client.post(url, data)
         self.assertRedirects(response, reverse('create_growth_d180_readings'))
+
+    def test_readings_empty_data(self):
+        """
+        Test a post where no data is sent.
+        """
+        mommy.make(D180Growth)
+        url = reverse('create_growth_d180_readings')
+        with self.assertRaises(ValidationError) as cm:
+            self.client.post(url, {})
+        exception = cm.exception
+        self.assertEqual(exception.message,
+            'ManagementForm data is missing or has been tampered with')
+
+    def test_readings_management_only(self):
+        """
+        Test a post where only managementform data is passed.
+        """
+        mommy.make(D180Growth)
+        url = reverse('create_growth_d180_readings')
+        data = {
+            'reading-INITIAL_FORMS': '0',
+            'reading-MAX_NUM_FORMS': '',
+            'reading-TOTAL_FORMS': '1',
+        }
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse('create_growth_d180_readings'))
+
+    def test_readings_readings_formset_invalid(self):
+        mommy.make(D180Growth)
+        url = reverse('create_growth_d180_readings')
+        data = {
+            'reading-INITIAL_FORMS': '0',
+            'reading-MAX_NUM_FORMS': '',
+            'reading-TOTAL_FORMS': '1',
+            'reading-0-layer_desc': 'test',
+        }
+        response = self.client.post(url, data)
+        self.assertFormsetError(response, 'readings_formset', 0, 'layer',
+            'This field is required.')
+
+    def test_readings_readings_formset_valid(self):
+        growth = mommy.make(D180Growth)
+        url = reverse('create_growth_d180_readings')
+        data = {
+            'reading-INITIAL_FORMS': '0',
+            'reading-MAX_NUM_FORMS': '',
+            'reading-TOTAL_FORMS': '1',
+            'reading-0-alkyl_flow_inner': '0.0',
+            'reading-0-alkyl_flow_middle': '0.0',
+            'reading-0-alkyl_flow_outer': '0.0',
+            'reading-0-alkyl_push_inner': '0.0',
+            'reading-0-alkyl_push_middle': '0.0',
+            'reading-0-alkyl_push_outer': '0.0',
+            'reading-0-cp2mg_dilution': '0.0',
+            'reading-0-cp2mg_flow': '0.0',
+            'reading-0-cp2mg_pressure': '0.0',
+            'reading-0-current_in': '0.0',
+            'reading-0-current_out': '0.0',
+            'reading-0-ecp_temp': '0.0',
+            'reading-0-gc_position': '0.0',
+            'reading-0-gc_pressure': '0.0',
+            'reading-0-h2_flow': '0.0',
+            'reading-0-hydride_inner': '0.0',
+            'reading-0-hydride_outer': '0.0',
+            'reading-0-hydride_pressure': '0.0',
+            'reading-0-layer': '1',
+            'reading-0-layer_desc': 'test',
+            'reading-0-motor_rpm': '0.0',
+            'reading-0-n2_flow': '0.0',
+            'reading-0-nh3_flow': '0.0',
+            'reading-0-pyro_in': '0.0',
+            'reading-0-pyro_out': '0.0',
+            'reading-0-silane_dilution': '0.0',
+            'reading-0-silane_flow': '0.0',
+            'reading-0-silane_mix': '0.0',
+            'reading-0-silane_pressure': '0.0',
+            'reading-0-tc_in': '0.0',
+            'reading-0-tc_out': '0.0',
+            'reading-0-tega2_flow': '0.0',
+            'reading-0-tega2_pressure': '0.0',
+            'reading-0-tmal1_flow': '0.0',
+            'reading-0-tmal1_pressure': '0.0',
+            'reading-0-tmga1_flow': '0.0',
+            'reading-0-tmga1_pressure': '0.0',
+            'reading-0-tmga2_flow': '0.0',
+            'reading-0-tmga2_pressure': '0.0',
+            'reading-0-tmin1_flow': '0.0',
+            'reading-0-tmin1_pressure': '0.0',
+            'reading-0-top_vp_flow': '0.0',
+            'reading-0-voltage_in': '0.0',
+            'reading-0-voltage_out': '0.0',
+        }
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse('create_growth_d180_readings'))
+        self.assertEqual(growth.readings.count(), 1)
+        self.assertEqual(growth.readings.first().layer_desc, 'test')
+
+    def test_postrun_empty_data(self):
+        """
+        Test a post where no data is sent.
+        """
+        mommy.make(D180Growth)
+        url = reverse('create_growth_d180_postrun')
+        response = self.client.post(url, {})
+        self.assertFormError(response, 'source_form', 'cp2mg',
+            'This field is required.')
+        self.assertFormError(response, 'checklist_form', 'field_0',
+            'This field is required.')
+
+    def test_postrun_valid(self):
+        mommy.make(D180Growth)
+        url = reverse('create_growth_d180_postrun')
+        data = {
+            'checklist-field_0': 'on',
+            'checklist-field_1': 'on',
+            'checklist-field_2': 'on',
+            'checklist-field_3': 'on',
+            'checklist-field_4': 'on',
+            'checklist-field_5': 'on',
+            'checklist-field_6': 'on',
+            'checklist-field_7': 'on',
+            'checklist-field_8': 'on',
+            'checklist-field_9': 'on',
+            'source-cp2mg': '0.00',
+            'source-nh3': '0.00',
+            'source-sih4': '0.00',
+            'source-tega1': '0.00',
+            'source-tmal1': '0.00',
+            'source-tmga1': '0.00',
+            'source-tmga2': '0.00',
+            'source-tmin1': '0.00',
+            'source-tmin2': '0.00',
+        }
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse('home'))
