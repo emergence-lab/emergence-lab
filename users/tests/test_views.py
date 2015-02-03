3d438fd212ceeaf6622be6ceb57f743cc93e4530
Neil Newman <nnewman2@albany.edu>
2015-02-03 17:39:59 -0500

    Added test for users app

---
+++ users/tests/test_views.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse, resolve
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+from users.urls import urlpatterns
+
+
+class TestUserAppViews(TestCase):
+
+    def setUp(self):
+        self.user_obj = get_user_model().objects.create_user('default', password='')
+        self.client.login(username='default', password='')
+
+    def test_profile_view(self):
+        """
+        Tests user profile view
+        """
+        url = reverse('users_profile', kwargs={'username': self.user_obj.username})
+        match = resolve('/accounts/users/{}'.format(self.user_obj.username))
+        response = self.client.get(url)
+        self.assertEqual(match.url_name, 'users_profile')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/profile_view.html')
