42bf8fe1ce73cd02ef3e2e0350221c64133e784f
Jonathan Marini <j.marini@ieee.org>
2015-02-07 14:03:24 -0500

    Cleanup of dashboard app.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,13 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
 from django.conf.urls import url
-from django.views.decorators.cache import never_cache
 
 from . import views
 
 
 urlpatterns = [
     url(r'^$', views.Dashboard.as_view(), name='dashboard'),
-    url(r'^add_action/$', never_cache(views.AddActionItemView.as_view()), name='add_action_item'),
-    url(r'^delete_action/(?P<action_item>\d+)$', never_cache(views.RemoveActionItemView.as_view()), name='remove_action_item'),
-    url(r'^(?P<slug>[\w-]+)/$', views.ProjectDetailDashboardView.as_view(), name='project_detail_dashboard'),
-    url(r'^(?P<project>[\w-]+)/(?P<slug>[\w-]+)/$', views.InvestigationDetailDashboardView.as_view(), name='investigation_detail_dashboard'),
+    url(r'^add_action/$',
+        views.AddActionItemView.as_view(), name='add_action_item'),
+    url(r'^delete_action/(?P<action_item>\d+)$',
+        views.RemoveActionItemView.as_view(), name='remove_action_item'),
+    url(r'^(?P<slug>[\w-]+)/$',
+        views.ProjectDetailDashboardView.as_view(),
+        name='project_detail_dashboard'),
+    url(r'^(?P<project>[\w-]+)/(?P<slug>[\w-]+)/$',
+        views.InvestigationDetailDashboardView.as_view(),
+        name='investigation_detail_dashboard'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,20 +1,23 @@
-from django.views.generic import DetailView, RedirectView
-from django.conf import settings
-from django.core.urlresolvers import reverse
-from django.utils import timezone, formats
-from django.shortcuts import HttpResponseRedirect
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
 import pickle
 import uuid
+
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.utils import timezone
+from django.views.generic import DetailView
 
 from braces.views import LoginRequiredMixin
 from redis import StrictRedis
 
 from core.models import User, Project, Investigation
 from core.streams import project_stream, investigation_stream
+from core.views import ActionReloadView
 from d180.models import D180Growth
 from schedule_queue.models import Reservation, tools
 from users.redis_config import ActionItem
-from messaging.redis_config import Helper
 
 
 class DashboardMixin(object):
@@ -23,19 +26,25 @@ class DashboardMixin(object):
     as well as user-context items.
     """
     def get_context_data(self, **kwargs):
-        projects = User.objects.filter(pk=self.request.user.id).values_list('projects__id', flat=True)
+        projects = (User.objects.filter(pk=self.request.user.id)
+                                .values_list('projects__id', flat=True))
         kwargs['active_projects'] = Project.active_objects.filter(id__in=projects)
         kwargs['inactive_projects'] = Project.inactive_objects.filter(id__in=projects)
         reservation_list = []
         for i in tools.get_tool_list():
-            tmp_res = Reservation.objects.filter(is_active=True, tool=i).order_by('priority_field').first()
+            tmp_res = (Reservation.objects.filter(is_active=True, tool=i)
+                                          .order_by('priority_field').first())
             if tmp_res and tmp_res.user == self.request.user:
-                tmp_res.url = tools.get_tool_info(i).get('process_start_url', None)
+                tmp_res.url = tools.get_tool_info(i).get('process_start_url',
+                                                         None)
                 reservation_list.append(tmp_res)
         kwargs['reservations'] = reservation_list
-        r=StrictRedis(settings.REDIS_HOST,settings.REDIS_PORT,settings.REDIS_DB)
+        r = StrictRedis(settings.REDIS_HOST,
+                        settings.REDIS_PORT,
+                        settings.REDIS_DB)
         kwargs['action_items'] = []
-        for i in r.lrange('users:{0}:action.items'.format(self.request.user.id), 0, -1):
+        for i in r.lrange('users:{0}:action.items'.format(self.request.user.id),
+                          0, -1):
             kwargs['action_items'].append(pickle.loads(i))
         return super(DashboardMixin, self).get_context_data(**kwargs)
 
@@ -50,7 +59,8 @@ class Dashboard(LoginRequiredMixin, DashboardMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(Dashboard, self).get_context_data(**kwargs)
-        context['growths'] = D180Growth.objects.filter(user=self.object).order_by('-uuid_full')[:25]
+        context['growths'] = (D180Growth.objects.filter(user=self.object)
+                                                .order_by('-uuid_full')[:25])
         context['tools'] = tools.get_tool_list()
         return context
 
@@ -67,8 +77,6 @@ class ProjectDetailDashboardView(LoginRequiredMixin, DashboardMixin, DetailView)
 
     def get_context_data(self, **kwargs):
         context = super(ProjectDetailDashboardView, self).get_context_data(**kwargs)
-        #context['growths'] = (D180Growth.objects.filter(project=self.object)
-        #                                        .order_by('-uuid_full')[:25])
         context['stream'] = project_stream(self.object)
         return context
 
@@ -82,38 +90,52 @@ class InvestigationDetailDashboardView(LoginRequiredMixin, DashboardMixin, Detai
 
     def get_context_data(self, **kwargs):
         context = super(InvestigationDetailDashboardView, self).get_context_data(**kwargs)
-        #context['growths'] = (D180Growth.objects.filter(investigation=self.object)
-        #                                        .order_by('-uuid_full')[:25])
         context['project'] = self.object.project
         context['stream'] = investigation_stream(self.object)
         return context
 
-class AddActionItemView(LoginRequiredMixin, DashboardMixin, RedirectView):
+
+class AddActionItemView(LoginRequiredMixin, DashboardMixin, ActionReloadView):
     """
     View for adding action item via Redis
     """
-    def get_redirect_url(self, *args, **kwargs):
+
+    def perform_action(self, request, *args, **kwargs):
         action_item = ActionItem()
         action_item.comment = str(self.request.POST.get('comment'))
-        r = StrictRedis(settings.REDIS_HOST,settings.REDIS_PORT,settings.REDIS_DB)
+        r = StrictRedis(settings.REDIS_HOST,
+                        settings.REDIS_PORT,
+                        settings.REDIS_DB)
         if self.request.POST.get('update_field'):
             index = self.request.POST.get('update_field')
-            original = pickle.loads(r.lindex('users:{0}:action.items'.format(self.request.user.id), index))
+            original = pickle.loads(
+                r.lindex('users:{}:action.items'.format(self.request.user.id),
+                         index))
             original.comment = action_item.comment
-            r.lset('users:{0}:action.items'.format(self.request.user.id), index, pickle.dumps(original))
+            r.lset('users:{}:action.items'.format(self.request.user.id),
+                   index, pickle.dumps(original))
         else:
             action_item.created = timezone.datetime.now()
-            r.lpush('users:{0}:action.items'.format(self.request.user.id), pickle.dumps(action_item))
+            r.lpush('users:{}:action.items'.format(self.request.user.id),
+                    pickle.dumps(action_item))
+
+    def get_redirect_url(self, *args, **kwargs):
         return str(reverse('dashboard') + "#action_items")
 
-class RemoveActionItemView(LoginRequiredMixin, DashboardMixin, RedirectView):
+
+class RemoveActionItemView(LoginRequiredMixin, DashboardMixin, ActionReloadView):
     """
     View for adding action item via Redis
     """
-    def get_redirect_url(self, *args, **kwargs):
-        r = StrictRedis(settings.REDIS_HOST,settings.REDIS_PORT,settings.REDIS_DB)
+
+    def perform_action(self, request, *args, **kwargs):
+        r = StrictRedis(settings.REDIS_HOST,
+                        settings.REDIS_PORT,
+                        settings.REDIS_DB)
         index = kwargs['action_item']
         tmp = uuid.uuid4().hex
         r.lset('users:{0}:action.items'.format(self.request.user.id), index, tmp)
         r.lrem('users:{0}:action.items'.format(self.request.user.id), 0, tmp)
+
+    def get_redirect_url(self, *args, **kwargs):
         return str(reverse('dashboard') + "#action_items")
