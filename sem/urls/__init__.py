1489ef545326d6f425f8b62aa3248c0dd2657c8b
Neil Newman <nnewman2@albany.edu>
2015-02-16 14:43:07 -0500

    Updated SEM urls

---
+++ sem/urls/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.conf.urls import url
+
+import sem.views
+
+
+urlpatterns = [
+    url(r'^$', sem.views.SEMList.as_view(), name='sem_list'),
+    url(r'^create/$', sem.views.SEMCreate.as_view(), name='sem_create'),
+    url(r'^(?P<pk>\d+)/$', sem.views.SEMDetail.as_view(), name='sem_detail'),
+    url(r'^(?P<pk>\d+)/update/$',
+        sem.views.SEMUpdate.as_view(), name='sem_update'),
+    url(r'^(?P<pk>\d+)/delete/$',
+        sem.views.SEMDelete.as_view(), name='sem_delete'),
+]
---
+++ sem/urls/api/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.conf.urls import url
+
+import sem.api
+
+
+urlpatterns = [
+    url(r'^$', sem.api.SEMListCreateAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/$', sem.api.SEMRetrieveUpdateAPIView.as_view()),
+]
