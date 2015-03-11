963414e8c5a3162a2d31eefe47f8ba04418a13ec
Neil Newman <nnewman2@albany.edu>
2015-03-11 18:10:29 -0400

    Exposed DataFile model in core.models.__init__

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -5,4 +5,4 @@ from .mixins import ActiveStateMixin, TimestampMixin, AutoUUIDMixin, UUIDMixin
 from .project import Project, Investigation, ProjectTracking
 from .user import User
 from .sample import Sample, Substrate
-from .process import Process, ProcessNode, SplitProcess
+from .process import Process, ProcessNode, SplitProcess, DataFile
