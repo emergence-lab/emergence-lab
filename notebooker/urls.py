23d405b0b5a635422b4cc8e104e55c83267cde05
Neil Newman <nnewman2@albany.edu>
2015-03-18 17:53:39 -0400

    Code cleanup post-Jupyter integration

--- notebooker/urls.py
+++ notebooker/urls.py
@@ -4,19 +4,9 @@ from django.views.decorators.cache import never_cache
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
-    #url(r'^new/$', login_required(views.ReservationCreate.as_view()), name='reservation_create'),
-    #url(r'^edit/(?P<pk>\d+)/$', login_required(views.ReservationEdit.as_view()), name='reservation_edit'),
-    #url(r'^cancel/(?P<pk>\d+)/$', login_required(never_cache(views.CancelReservation.as_view())), name='cancel_reservation'),
-    #url(r'^close/(?P<pk>\d+)/$', login_required(never_cache(views.CloseReservation.as_view())), name='close_reservation'),
-    #url(r'^increase/(?P<pk>\d+)/$', login_required(never_cache(views.IncreasePriority.as_view())), name='increase_priority'),
-    #url(r'^decrease/(?P<pk>\d+)/$', login_required(never_cache(views.DecreasePriority.as_view())), name='decrease_priority'),
-    #url(r'^(?P<tool_slug>[a-z0-9\-]+)/$', login_required(never_cache(views.ReservationListByTool.as_view())), name='reservation_list_by_tool'),
-    #url(r'^edit/(?P<notebook_name>[a-z0-9\w-]+)/(?P<cell>\d+)$', login_required(never_cache(views.CellEdit.as_view())), name='notebook_cell_edit'),
-    url(r'^embedded$', login_required(never_cache(views.EmbeddedNotebook.as_view())), name='notebook_embedded'),
-
-    url(r'^edit/(?P<notebook_name>[A-Za-z0-9\-]+)$', login_required(never_cache(views.NotebookInteractive.as_view())), name='notebook_int'),
-
-    url(r'^view/(?P<notebook_name>[a-z0-9\w-]+)$', login_required(views.NotebookViewer.as_view()), name='notebook_viewer'),
-
-    url(r'^$', login_required(views.NotebookList.as_view()), name='notebook_list'),
+    url(r'^view/(?P<notebook_name>[a-z0-9\w-]+)$',
+        login_required(views.NotebookViewer.as_view()),
+        name='notebook_viewer'),
+    url(r'^$', login_required(views.NotebookList.as_view()),
+        name='notebook_list'),
 )
--- notebooker/views.py
+++ notebooker/views.py
@@ -2,24 +2,13 @@ from django.shortcuts import render, HttpResponse, HttpResponseRedirect, render_
 from django.core.urlresolvers import reverse
 import os
 from django.conf import settings
-from django.views.generic import ListView, RedirectView, TemplateView, FormView
-import tempfile
+from django.views.generic import TemplateView
 import json
-import platform
-from time import sleep
 
 from IPython.nbconvert.exporters.html import HTMLExporter
 from IPython.config import Config
-from IPython.nbconvert.preprocessors import ExecutePreprocessor
-#from IPython.kernel import KernelManager
-#from IPython import get_ipython
 from .runipy_extender import NotebookRunner
-#from runipy.notebook_runner import NotebookRunner
 from IPython.nbformat import read, write, reads
-from .forms import NBCellEdit
-
-
-# Create your views here.
 
 
 class EmbeddedNotebook(TemplateView):
@@ -46,9 +35,7 @@ class NotebookList(TemplateView):
         return super(NotebookList, self).get_context_data(**kwargs)
 
 class NotebookViewer(TemplateView):
-    template_name = 'notebooker/demo_nb.html'
-
-
+    template_name = 'notebooker/nb_viewer.html'
 
     def get_context_data(self, **kwargs):
         filename = kwargs['notebook_name']
@@ -57,11 +44,7 @@ class NotebookViewer(TemplateView):
         e = HTMLExporter(config=Config({'HTMLExporter':{'default_template':'basic'}}))
         path = os.path.join(settings.MEDIA_ROOT, 'notebooks', user, str(filename + '.ipynb'))
         if 'notebook_content' not in kwargs:
-
-            #kwargs['notebook_content'] = str(e.from_filename(path))
-
             with open(tempfile.NamedTemporaryFile().name, 'wb+') as output:
-            #with open(os.path.join(settings.MEDIA_ROOT, 'notebooks', user, 'out1.html'), 'wb') as output:
                 for i in e.from_filename(path):
                     if not str(i).startswith('defaultdict'):
                         output.write(i)
@@ -77,187 +60,3 @@ class NotebookViewer(TemplateView):
                     tmp.append(comments[i])
             kwargs['comments'] = tmp
         return super(NotebookViewer, self).get_context_data(**kwargs)
-
-class NotebookInteractive(FormView):
-    template_name = 'notebooker/interactive_nb.html'
-    form_class = NBCellEdit
-
-    def _set_ipython_variables(self, *args, **kwargs):
-        self.nb_path = os.path.join(settings.MEDIA_ROOT,
-                               'notebooks',
-                               str(self.request.user),
-                               str(self.kwargs['notebook_name'] + '.ipynb'))
-        self.nb_name = self.kwargs['notebook_name']
-        #self.e = HTMLExporter(config=Config({'HTMLExporter':{'default_template':'basic'}}))
-        self.e = HTMLExporter()
-        self.nb = read(open(self.nb_path, 'rb'), 4)
-        self.cells = self.nb.cells
-
-    def get(self, request, *args, **kwargs):
-        self._set_ipython_variables()
-        return super(NotebookInteractive, self).get(request)
-
-    def get_form_kwargs(self):
-        kwargs = super(NotebookInteractive, self).get_form_kwargs()
-        if 'cell_count' not in kwargs:
-            kwargs['cell_count'] = int(len(self.cells[0]))
-        return kwargs
-
-    def get_initial(self):
-        initial = super(NotebookInteractive, self).get_initial()
-        for cell_num, i in enumerate(self.cells):
-            if i['cell_type'] == 'code':
-                initial['cell_{}'.format(cell_num)] = i['source']
-                initial['type_{}'.format(cell_num)] = i['cell_type']
-            elif i['cell_type'] == 'markdown':
-                initial['cell_{}'.format(cell_num)] = i['source']
-                initial['type_{}'.format(cell_num)] = i['cell_type']
-        return initial
-
-    def get_context_data(self, **kwargs):
-        if 'notebook_content' not in kwargs:
-            nb_content = []
-            for cell in self.cells:
-                #with open(tempfile.NamedTemporaryFile().name, 'wb+') as output:
-                temp_nb = {
-                    'metadata': {},
-                    'nbformat': 4,
-                    'cells': [cell]
-                }
-                for i in self.e.from_notebook_node(reads(json.dumps(temp_nb), 4)):
-                    if not 'defaultdict' in str(i):
-                        nb_content.append(i)
-            kwargs['notebook_content'] = nb_content
-        if 'comments' not in kwargs:
-            tmp = []
-            with open(os.path.join(settings.MEDIA_ROOT, 'notebooks', str(self.request.user), 'test.json'), 'r') as comment:
-                comments = json.load(comment)
-                for i in comments:
-                    tmp.append(comments[i])
-            kwargs['comments'] = tmp
-        if 'cell_count' not in kwargs:
-            kwargs['cell_count'] = int(len(self.cells))
-        if 'notebook_name' not in kwargs:
-            kwargs['notebook_name'] = self.nb_name
-        return super(NotebookInteractive, self).get_context_data(**kwargs)
-
-    def post(self, request, *args, **kwargs):
-        self._set_ipython_variables()
-        return super(NotebookInteractive, self).post(request)
-
-    def form_valid(self, form):
-        cell_count = int(len(self.cells[0]))
-        for i in range(cell_count):
-            if 'upd_cell_{}'.format(int(i + 1)) in self.request.POST:
-                cell_num = int(i)
-            elif 'upd_all' in self.request.POST:
-                pass
-        if self.cells[cell_num]['cell_type'] == 'code':
-            self.cells[cell_num]['source'] = form.cleaned_data['cell_{}'.format(cell_num)]
-        elif self.cells[cell_num]['cell_type'] == 'markdown':
-            self.cells[cell_num]['source'] = form.cleaned_data['cell_{}'.format(cell_num)]
-        r = NotebookRunner(self.nb)
-        for cell in self.cells:
-            if cell['cell_type'] == 'code':
-                r.run_cell(cell)
-        write(self.nb, open(self.nb_path, 'w'), 4)
-        return HttpResponseRedirect(reverse('notebook_int', kwargs = {'notebook_name': self.kwargs['notebook_name']}))
-
-#class NotebookIntDemo(TemplateView):
-#    template_name = 'notebooker/demo_int_nb.html'
-#
-#    def get_context_data(self, **kwargs):
-#        filename = kwargs['notebook_name']
-#        user = str(self.request.user)
-#        e = HTMLExporter(config=Config({'HTMLExporter':{'default_template':'basic'}}))
-#        path = os.path.join(settings.MEDIA_ROOT, 'notebooks', user, str(filename + '.ipynb'))
-#        with open(path, 'r') as nb:
-#            notebook = json.loads(nb.read())
-#        if 'notebook_content' not in kwargs:
-#            cntnt = []
-#            for cell in notebook['worksheets'][0]['cells']:
-#                with open(tempfile.NamedTemporaryFile().name, 'wb+') as output:
-#                    m = {}
-#                    t = []
-#                    t.append(cell)
-#                    m['cells'] = t
-#                    c = []
-#                    c.append(m)
-#                    p = {}
-#                    p['metadata'] = {}
-#                    p['nbformat'] = 3
-#                    p['worksheets'] = c
-#                    output.write(json.dumps(p))
-#                    output.seek(0)
-#                    with open (tempfile.NamedTemporaryFile().name, 'wb+') as cleaned_output:
-#                        for i in e.from_file(output):
-#                            if 'defaultdict' not in str(i):
-#                                cleaned_output.write(i)
-#                        cleaned_output.flush()
-#                        cleaned_output.seek(0)
-#                        cntnt.append(cleaned_output.read())
-#                        cleaned_output.close()
-#                    output.close()
-#            kwargs['notebook_content'] = cntnt
-#        if 'comments' not in kwargs:
-#            tmp = []
-#            with open(os.path.join(settings.MEDIA_ROOT, 'notebooks', user, 'test.json'), 'r') as comment:
-#                comments = json.load(comment)
-#                for i in comments:
-#                    tmp.append(comments[i])
-#            kwargs['comments'] = tmp
-#        return super(NotebookIntDemo, self).get_context_data(**kwargs)
-
-class CellEdit(FormView):
-    template_name = 'notebooker/cell_edit_form.html'
-    form_class = NBCellEdit
-    success_url = '/ipython'
-    def get_initial(self):
-        initial = super(CellEdit, self).get_initial()
-        cell_num = int(self.kwargs['cell'])
-        user = str(self.request.user)
-        nb = json.load(open(os.path.join(settings.MEDIA_ROOT, 'notebooks', user, str(self.kwargs['notebook_name'] + '.ipynb')), 'r'))
-        if nb['worksheets'][0]['cells'][cell_num]['cell_type'] == 'code':
-            tmp = nb['worksheets'][0]['cells'][cell_num]['input']
-        elif nb['worksheets'][0]['cells'][cell_num]['cell_type'] == 'markdown':
-            tmp = nb['worksheets'][0]['cells'][cell_num]['source']
-        #initial['cell'] = nb['worksheets'][0]['cells'][cell_num]['input']
-        with open(tempfile.NamedTemporaryFile().name, 'wb+') as output:
-            for i in tmp:
-                output.write(i)
-            output.flush()
-            output.seek(0)
-            initial['cell'] = output.read()
-            output.close
-        return initial
-
-    def form_valid(self, form):
-        user = str(self.request.user)
-        cell_num = int(self.kwargs['cell'])
-        nb_path = os.path.join(settings.MEDIA_ROOT, 'notebooks', user, str(self.kwargs['notebook_name'] + '.ipynb'))
-        nb = json.load(open(nb_path, 'r'))
-        if nb['worksheets'][0]['cells'][cell_num]['cell_type'] == 'code':
-            nb['worksheets'][0]['cells'][cell_num]['input'] = form.cleaned_data['cell']
-        elif nb['worksheets'][0]['cells'][cell_num]['cell_type'] == 'markdown':
-            nb['worksheets'][0]['cells'][cell_num]['source'] = form.cleaned_data['cell']
-        with open(os.path.join(settings.MEDIA_ROOT, 'notebooks', user, str(self.kwargs['notebook_name'] + '.ipynb')), 'wb+') as output:
-            try:
-                output.truncate()
-                output.seek(0)
-                json.dump(nb, output)
-                output.close()
-            except Exception as e: print(e)
-        nb = read(open(nb_path), 'json')
-        r = NotebookRunner(nb)
-        for i, cell in enumerate(r.iter_code_cells()):
-            if i == cell_num:
-                try:
-                    r.run_cell(cell)
-                except NotebookError:
-                    if not skip_exceptions:
-                        raise
-                #if progress_callback:
-                #    progress_callback(i)
-        #r.run_notebook(skip_exceptions=True)
-        write(r.nb, open(nb_path, 'w'), 'json')
-        return HttpResponseRedirect(reverse('notebook_int_demo', kwargs = {'notebook_name': self.kwargs['notebook_name']}))
