e971cb082a8e4b6a9b5950222e4107f602df0940
Jonathan Marini <j.marini@ieee.org>
2015-03-24 10:49:30 -0400

    Moved extracted data from AFMScan to AFMFile.

--- afm/migrations/0001_initial.py
+++ afm/migrations/0001_initial.py
@@ -2,27 +2,34 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import afm.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0001_initial'),
+        ('core', '0005_auto_20150324_1032'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='AFMScan',
+            name='AFMFile',
             fields=[
-                ('process_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='core.Process')),
+                ('datafile_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='core.DataFile')),
                 ('scan_number', models.IntegerField(default=0)),
                 ('rms', models.DecimalField(max_digits=7, decimal_places=3)),
                 ('zrange', models.DecimalField(max_digits=7, decimal_places=3)),
                 ('location', models.CharField(default='c', max_length=45, choices=[('c', 'Center'), ('r', 'Round'), ('f', 'Flat')])),
                 ('size', models.DecimalField(max_digits=7, decimal_places=3)),
-                ('height', models.ImageField(max_length=150, null=True, upload_to=afm.models.get_afm_path, blank=True)),
-                ('amplitude', models.ImageField(max_length=150, null=True, upload_to=afm.models.get_afm_path, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('core.datafile',),
+        ),
+        migrations.CreateModel(
+            name='AFMScan',
+            fields=[
+                ('process_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='core.Process')),
             ],
             options={
                 'abstract': False,
