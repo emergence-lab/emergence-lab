6c7a75ec054198777cfe81b1cbd1a34ec49628a6
Neil Newman <nnewman2@albany.edu>
2015-03-24 10:00:17 -0400

    Updated template and fixed view for Hall detail view

--- hall/views.py
+++ hall/views.py
@@ -16,7 +16,7 @@ class HallDetailView(generic.DetailView):
     template_name = 'hall/hall_detail.html'
 
     def get_context_data(self, **kwargs):
-        context = super(SEMDetail, self).get_context_data(**kwargs)
+        context = super(HallDetailView, self).get_context_data(**kwargs)
         process = Hall.objects.get(id=self.kwargs['pk'])
         context['process_id'] = process.id
         context['dataset'] = [i.halldata for i in process.datafiles.get_queryset()]
