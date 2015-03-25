3920a64ef2cfe872d5be420efc5013b97272e712
Jonathan Marini <j.marini@ieee.org>
2015-03-25 10:58:09 -0400

    Initial fixing of views/templates for afm file changes.

--- afm/views.py
+++ afm/views.py
@@ -2,11 +2,13 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
 from braces.views import LoginRequiredMixin
 
-from .models import AFMFile, AFMScan
+from afm.models import AFMFile, AFMScan
+from core.models import Process
 
 
 class AFMList(LoginRequiredMixin, ListView):
@@ -24,6 +26,18 @@ class AFMDetail(LoginRequiredMixin, DetailView):
     """
     model = AFMScan
     template_name = 'afm/afm_detail.html'
+    context_object_name = 'afm'
+    lookup_url_kwarg = 'uuid'
+
+    def get_object(self, queryset=None):
+        queryset = queryset or self.get_queryset()
+
+        assert self.lookup_url_kwarg in self.kwargs, (
+            'Expected view {} to be called with a URL keyword argument '
+            'named "{}". Fix your URL conf.'.format(self.__class__.__name__,
+                                                    self.lookup_url_kwarg))
+        uuid = Process.strip_uuid(self.kwargs[self.lookup_url_kwarg])
+        return get_object_or_404(Process, uuid_full__startswith=uuid)
 
     def get_context_data(self, **kwargs):
         context = super(AFMDetail, self).get_context_data(**kwargs)
@@ -41,6 +55,9 @@ class AFMCreate(LoginRequiredMixin, CreateView):
     model = AFMScan
     template_name = 'afm/afm_create.html'
 
+    def get_success_url(self):
+        return reverse('afm_detail', args=(self.object.uuid,))
+
 
 class AFMUpdate(LoginRequiredMixin, UpdateView):
     """
@@ -48,6 +65,22 @@ class AFMUpdate(LoginRequiredMixin, UpdateView):
     """
     model = AFMScan
     template_name = 'afm/afm_update.html'
+    context_object_name = 'afm'
+    lookup_url_kwarg = 'uuid'
+    fields = ('comment',)
+
+    def get_object(self, queryset=None):
+        queryset = queryset or self.get_queryset()
+
+        assert self.lookup_url_kwarg in self.kwargs, (
+            'Expected view {} to be called with a URL keyword argument '
+            'named "{}". Fix your URL conf.'.format(self.__class__.__name__,
+                                                    self.lookup_url_kwarg))
+        uuid = Process.strip_uuid(self.kwargs[self.lookup_url_kwarg])
+        return get_object_or_404(Process, uuid_full__startswith=uuid)
+
+    def get_success_url(self):
+        return reverse('afm_detail', args=(self.object.uuid,))
 
 
 class AFMDelete(LoginRequiredMixin, DeleteView):
@@ -56,6 +89,18 @@ class AFMDelete(LoginRequiredMixin, DeleteView):
     """
     model = AFMScan
     template_name = 'afm/afm_delete.html'
+    context_object_name = 'afm'
+    lookup_url_kwarg = 'uuid'
+
+    def get_object(self, queryset=None):
+        queryset = queryset or self.get_queryset()
+
+        assert self.lookup_url_kwarg in self.kwargs, (
+            'Expected view {} to be called with a URL keyword argument '
+            'named "{}". Fix your URL conf.'.format(self.__class__.__name__,
+                                                    self.lookup_url_kwarg))
+        uuid = Process.strip_uuid(self.kwargs[self.lookup_url_kwarg])
+        return get_object_or_404(Process, uuid_full__startswith=uuid)
 
     def get_success_url(self):
         return reverse('afm_list')
