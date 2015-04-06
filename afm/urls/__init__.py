edcd7dbd64b6032f0b4c2bfe3990231ec46b952a
Jonathan Marini <j.marini@ieee.org>
2015-04-06 11:33:10 -0400

    Added ability to add afm scan to sample.

--- afm/urls/__init__.py
+++ afm/urls/__init__.py
@@ -19,4 +19,6 @@ urlpatterns = [
         afm.views.AFMFileUpload.as_view(), name='afm_upload'),
     url(r'^(?P<uuid>p[0-9a-f\-]{7,})/remove/(?P<id>[0-9]+)/$',
         afm.views.AFMRemoveFileActionReloadView.as_view(), name='afm_file_remove'),
+    url(r'^autocreate/(?P<uuid>s[0-9]+)(?P<piece>[a-z]+)?/$',
+        afm.views.AutocreateAFMView.as_view(), name='afm_autocreate'),
 ]
