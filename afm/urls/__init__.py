c6fb07492550fc06753ddbf3fd1df0993a372816
Jonathan Marini <j.marini@ieee.org>
2015-04-09 11:24:23 -0400

    Removed unneeded afm views and templates.

--- afm/urls/__init__.py
+++ afm/urls/__init__.py
@@ -7,14 +7,6 @@ import afm.views
 
 
 urlpatterns = [
-    url(r'^$', afm.views.AFMList.as_view(), name='afm_list'),
-    url(r'^create/$', afm.views.AFMCreate.as_view(), name='afm_create'),
-    url(r'^(?P<uuid>p[0-9a-f\-]{7,})/$',
-        afm.views.AFMDetail.as_view(), name='afm_detail'),
-    url(r'^(?P<uuid>p[0-9a-f\-]{7,})/update/$',
-        afm.views.AFMUpdate.as_view(), name='afm_update'),
-    url(r'^(?P<uuid>p[0-9a-f\-]{7,})/delete/$',
-        afm.views.AFMDelete.as_view(), name='afm_delete'),
     url(r'^(?P<uuid>p[0-9a-f\-]{7,})/upload/$',
         afm.views.AFMFileUpload.as_view(), name='afm_upload'),
     url(r'^(?P<uuid>p[0-9a-f\-]{7,})/remove/(?P<id>[0-9]+)/$',
