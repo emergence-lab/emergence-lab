c52f217ebdd0396020b26dd362f9e81e1e2e3c1f
Jonathan Marini <j.marini@ieee.org>
2015-04-20 14:31:45 -0400

    Changed information for about page.

--- core/views/utility.py
+++ core/views/utility.py
@@ -146,15 +146,12 @@ class AboutView(generic.TemplateView):
             tag = subprocess.check_output(
                 ['git', 'describe', '--tags'],
                 cwd=settings.BASE_DIR).strip()
-            ahead = len(subprocess.check_output(
-                ['git', 'rev-list', '0.10.0..{}'.format(branch)]))
         except subprocess.CalledProcessError as e:
             raise e
             context['error'] = '{0}: {1}'.format(e.returncode,
                                                  e.output)
             return context
         context['tag'] = tag
-        context['commits_ahead'] = ahead
         context['commit'] = commit[:7]
         context['branch'] = branch
         return context
