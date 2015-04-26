bf9e5ba62bbb15b090e5266b3472329af166c05e
Jonathan Marini <j.marini@ieee.org>
2015-04-26 12:09:35 -0400

    Changed handling of uploaded files to use rq.

--- afm/views.py
+++ afm/views.py
@@ -1,24 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-import logging
-import math
-import os
-
-from django.conf import settings
-from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.urlresolvers import reverse
 
-import nanoscope
-from PIL import Image, ImageDraw, ImageFont
-import six
-
-from afm.models import AFMFile, AFMScan
 from afm.forms import AutoCreateAFMForm
+from afm.models import AFMFile, AFMScan
+from afm.tasks import process_nanoscope_file
 from core.views import CreateUploadProcessView, UploadFileView
-
-
-logger = logging.getLogger(__name__)
 
 
 class AFMFileUpload(UploadFileView):
@@ -26,95 +14,9 @@ class AFMFileUpload(UploadFileView):
     Add files to an existing afm process
     """
     model = AFMFile
-
-    def process_image(self, scan, filename, scan_number):
-        image = Image.fromarray(scan.colorize())
-        if image.size != (512, 512):
-            logger.debug('Image not correct size: {}, should be 512 x 512'.format(image.size))
-            image = image.resize((512, 512), resample=Image.NEAREST)
-            logger.debug('Resized image to 512 x 512')
-
-        processed_image = Image.new(image.mode, (654, 580), 'white')
-        processed_image.paste(image,
-                              (20, 20, image.size[0] + 20, image.size[1] + 20))
-
-        calibri = ImageFont.truetype(
-            os.path.join(settings.STATIC_ROOT, 'afm', 'fonts', 'calibrib.ttf'),
-            20)
-        draw = ImageDraw.Draw(processed_image)
-
-        scale_image = Image.open(
-            os.path.join(settings.STATIC_ROOT, 'afm', 'img', 'scale_12.png'))
-        processed_image.paste(scale_image,
-                             (28 + image.size[0],
-                              30,
-                              28 + image.size[0] + scale_image.size[0],
-                              30 + scale_image.size[1]))
-        if scan.type == 'Height':
-            unit = 'nm'
-        else:
-            unit = 'V'
-        draw.text(
-            (28 + image.size[0] + scale_image.size[0] + 7, 25),
-            '{0:.0f} {1}'.format(scan.height_scale, unit), 'black', calibri)
-        draw.text(
-            (28 + image.size[0] + scale_image.size[0] + 7, 20 + scale_image.size[1]),
-            '0.0 {}'.format(unit), 'black', calibri)
-
-        zrange_str = 'Z-Range: {0:.2f} nm'.format(scan.zrange)
-        rms_str = 'RMS: {0:.2f} nm'.format(scan.rms)
-        size_str = 'Area: {0} \u03bcm X {0} \u03bcm'.format(math.sqrt(scan.scan_area))
-
-        draw.text((20, 25 + image.size[1]), filename, 'black', calibri)
-        draw.text(
-            (20 + image.size[0] - calibri.getsize(size_str)[0], 25 + image.size[1]),
-            size_str, 'black', calibri)
-        if scan.type == 'Height':
-            draw.text(
-                (20, 50 + image.size[1]), zrange_str, 'black', calibri)
-            draw.text(
-                (20 + image.size[0] - calibri.getsize(rms_str)[0], 50 + image.size[1]),
-                rms_str, 'black', calibri)
-        else:
-            type_str = '{} AFM'.format(scan.type)
-            draw.text(
-                (20 + image.size[0] - calibri.getsize(type_str)[0], 50 + image.size[1]),
-                type_str, 'black', calibri)
-
-        tempio = six.StringIO()
-        processed_image.save(tempio, format='PNG')
-        return InMemoryUploadedFile(
-            tempio, field_name=None, name=filename + '.png',
-            content_type='image/png', size=tempio.len, charset=None)
-
-    def process_file(self, uploaded_file):
-        scan_number = int(os.path.splitext(uploaded_file.name)[-1][1:])
-        if '_' in uploaded_file.name:
-            location = os.path.splitext(uploaded_file.name)[0].split('_')[-1]
-            if location not in 'rRcCfFeE':
-                location = 'c'
-        else:
-            location = 'c'
-        raw = six.BytesIO(uploaded_file.read())
-        raw.mode = 'b'
-        scan = nanoscope.read(raw, encoding='cp1252')
-        logger.debug('Read in file {}'.format(uploaded_file.name))
-
-        processed_files = [(uploaded_file, dict(image_type='Raw', state='raw',
-                                                rms=0.0, zrange=0.0,
-                                                size=0.0, scan_number=scan_number,
-                                                content_type='application/octet-stream'))]
-        for img in scan:
-            img.process()
-            logger.debug('Converted and flattened {} scan'.format(img.type))
-            processed_image = self.process_image(img, uploaded_file.name, scan_number)
-            logger.debug('Created image file for {} scan'.format(img.type))
-            processed_files.append(
-                (processed_image, dict(image_type=img.type, state='extracted',
-                                       rms=img.rms, zrange=img.zrange,
-                                       size=math.sqrt(img.scan_area),
-                                       scan_number=scan_number)))
-        return processed_files
+    rq_config = {
+        'process': process_nanoscope_file,
+    }
 
 
 class AutocreateAFMView(CreateUploadProcessView):
---
+++ core/tasks.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.conf import settings
+
+from collections import namedtuple
+import logging
+import os
+
+import django_rq
+
+
+logger = logging.getLogger(__name__)
+
+
+AsyncDjangoFile = namedtuple('AsyncDjangoFile', ['data', 'kwargs'])
+
+
+@django_rq.job
+def process_file(raw_file):
+    return [AsyncDjangoFile(raw_file, {})]
+
+
+@django_rq.job
+def save_files(model, process, job_id, queue='default'):
+    queue = django_rq.get_queue(queue)
+    logger.debug('Retrieving results for job {} from queue {}'.format(
+        job_id, queue.name))
+    processed_files = queue.fetch_job(job_id).result
+
+    for f, kwargs in processed_files:
+        logger.debug('Saving file \'{}\' for process {}'.format(
+            f.name, process.uuid_full))
+
+        obj = model.objects.create(data=None, process=process, **kwargs)
+        obj.data = f
+        obj.save()
+        _save_sample_files(process, obj)
+
+
+def _save_sample_files(process, file_object):
+    """
+    Create hard links in sample-specific folders to the datafile in the
+    process-specific folder.
+
+    Create sample-specific directory structure:
+      samples/<sample.uuid>/<process.slug>/<process.uuid_full.hex>/
+    and create a hard link to the file in the process-specific
+    directory structure:
+      processes/<process.uuid_full.hex>/
+    """
+    for sample in process.samples(unique=True):
+        sample_dir = os.path.abspath(os.path.join(
+                settings.MEDIA_ROOT, 'samples', sample.uuid, process.slug))
+        target_dir = os.path.join(sample_dir, process.uuid_full.hex)
+        try:
+            os.makedirs(target_dir)
+            logger.debug('created directory {}'.format(target_dir))
+        except OSError:
+            pass
+        source_dir, filename = os.path.split(os.path.abspath(file_object.data.path))
+        target = os.path.join(target_dir, filename)
+        source = os.path.join(source_dir, filename)
+        logger.debug('linking {} to {}'.format(target, source))
+        os.link(source, target)
