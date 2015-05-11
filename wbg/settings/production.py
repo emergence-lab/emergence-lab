3e74760cce8ffae10b9954d9a97c08f1fec11a97
Jonathan Marini <j.marini@ieee.org>
2015-05-11 14:23:00 -0400

    Removed some production settings

--- wbg/settings/production.py
+++ wbg/settings/production.py
@@ -16,13 +16,3 @@ ALLOWED_HOSTS += get_secret('ALLOWED_HOSTS')
 MEDIA_URL = '/wsgi/media/'
 MEDIA_ROOT = get_secret('MEDIA_ROOT')
 SENDFILE_BACKEND = 'sendfile.backends.xsendfile'
-
-
-# Templates
-
-TEMPLATE_LOADERS = ('django.template.loaders.cached.Loader',) + TEMPLATE_LOADERS
-
-
-# Sessions
-
-SESSION_ENGINE 'django.contrib.sesstions.backend.cache'
