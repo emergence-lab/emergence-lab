45b2eeb1c69ffad7b4b030d49c038f26f26e274e
Jonathan Marini <j.marini@ieee.org>
2015-05-25 14:49:32 -0400

    Removed afm api urls.

--- afm/urls/__init__.py
+++ afm/urls/__init__.py
@@ -9,6 +9,6 @@ import afm.views
 urlpatterns = [
     url(r'^(?P<uuid>p[0-9a-f\-]{7,})/upload/$',
         afm.views.AFMFileUpload.as_view(), name='afm_upload'),
-    url(r'^autocreate/(?P<uuid>s[0-9]+)(?P<piece>[a-z]+)?/$',
+    url(r'^autocreate/(?P<uuid>s[0-9]+)/$',
         afm.views.AutocreateAFMView.as_view(), name='afm_autocreate'),
 ]
--- wbg/api/v0.py
+++ wbg/api/v0.py
@@ -12,7 +12,6 @@ urlpatterns = [
     url(r'^process/', include('core.urls.api.process')),
     url(r'^sample/', include('core.urls.api.sample')),
     url(r'^utility/', include('core.urls.api.utility')),
-    url(r'^afm/', include('afm.urls.api')),
     url(r'^sem/', include('sem.urls.api')),
     url(r'^messaging/', include('messaging.urls.api')),
     url(r'^d180/', include('d180.urls.api')),
