cdf71b8e678c1b19a3259ca154be2b1d953fc912
Neil Newman <nnewman2@albany.edu>
2015-06-02 13:47:31 -0400

    Fixes to literature model

--- project_management/models.py
+++ project_management/models.py
@@ -34,10 +34,21 @@ class Literature(TimestampMixin, models.Model):
     Scientific articles.
     """
     title = models.CharField(max_length=500)
-    external_id = models.CharField(max_length=100)
-    abstract = fields.RichTextField(_('abstract'), blank=True)
+    external_id = models.CharField(max_length=100, blank=True)
+    abstract = fields.RichTextField(_('abstract'), blank=True, null=True)
     doi_number = models.CharField(max_length=100, blank=True)
+    year = models.CharField(max_length=4, blank=True)
+    journal = models.CharField(max_length=200, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    investigations = models.ManyToManyField(Investigation,
+                                        related_name='literature',
+                                        related_query_name='literature',
+                                        null=True)
+    milestones = models.ManyToManyField(Milestone,
+                                        related_name='literature',
+                                        related_query_name='literature',
+                                        null=True)
+
 
 #
 # class Keyword(models.Model):
