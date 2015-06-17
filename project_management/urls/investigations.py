a8b4630d086ce77def9a21e48f0ec786ad410474
Neil Newman <nnewman2@albany.edu>
2015-06-17 15:53:52 -0400

    Changed to using slug for investigation urls in PM

--- project_management/urls/investigations.py
+++ project_management/urls/investigations.py
@@ -8,6 +8,6 @@ import project_management.views as views
 urlpatterns = [
     url(r'^$', views.InvestigationListView.as_view(),
         name='pm_investigation_list'),
-    url(r'^detail/(?P<pk>[0-9]+)$', views.InvestigationDetailView.as_view(),
+    url(r'^detail/(?P<slug>[\w-]+)$', views.InvestigationDetailView.as_view(),
         name='pm_investigation_detail'),
 ]
