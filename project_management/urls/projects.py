99c8813b127fba76569ce7b519ae09da53e86acf
Neil Newman <nnewman2@albany.edu>
2015-06-17 15:31:48 -0400

    Added project views and templates to PM

---
+++ project_management/urls/projects.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.conf.urls import url
+
+import project_management.views as views
+
+
+urlpatterns = [
+    url(r'^$', views.ProjectListView.as_view(),
+        name='pm_project_list'),
+]
