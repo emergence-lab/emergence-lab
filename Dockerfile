468e32f2d40a5198d443dd888b73f3e8ba186925
Neil Newman <nnewman2@albany.edu>
2015-11-21 17:18:23 -0500

    Adds basic docker support using docker-compose

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu:14.04
+RUN apt-get update && apt-get install -y build-essential git python python-dev python-setuptools supervisor libpq-dev libffi-dev libldap2-dev libsasl2-dev libssl-dev libxml2-dev libxslt1-dev libcairo2-dev libpango1.0-0 libgdk-pixbuf2.0-0
+RUN (easy_install pip &&\
+  pip install uwsgi)
+
+ADD . /opt/django
+RUN pip install -r /opt/django/requirements/development.txt
---
+++ wbg/settings/docker.py
@@ -0,0 +1,43 @@
+import logging
+
+from .base import *
+
+
+# Debugging
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+
+# Installed Apps
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+    'nplusone.ext.django',
+)
+
+
+# Authentication
+
+AUTH_LDAP_SERVER_URI = 'ldap://openldap:389'
+
+
+# Middleware
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'nplusone.ext.django.NPlusOneMiddleware',
+)
+
+
+# Logging
+
+NPLUSONE_LOGGER = logging.getLogger('nplusone')
+NPLUSONE_LOG_LEVEL = logging.WARN
+
+
+# Media Files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, os.pardir, 'media')
+SENDFILE_BACKEND = 'sendfile.backends.development'
